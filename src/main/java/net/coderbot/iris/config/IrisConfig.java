bcea58fa83c54094cde639e9695e157887949a93
coderbot <coderbot16@gmail.com>
2020-11-26 18:38:11 -0800

    Refactor configuration loading to be far more robust

--- src/main/java/net/coderbot/iris/config/IrisConfig.java
+++ src/main/java/net/coderbot/iris/config/IrisConfig.java
@@ -1,73 +1,57 @@
 package net.coderbot.iris.config;
 
 import net.fabricmc.loader.api.FabricLoader;
-import net.coderbot.iris.Iris;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 /**
  * A class dedicated to storing the config values of shaderpacks. Right now it only stores the path to the current shaderpack
  */
 public class IrisConfig {
-	private Path shaderpath;
-	private Path propertiesPath;
+	private static final String COMMENT =
+			"This file stores configuration options for Iris, such as the currently active shaderpack";
+
 	/**
-	 * Represents if the current loaded shaderpack is in internal one or not
+	 * The path to the current shaderpack. Null if the internal shaderpack is being used.
 	 */
-	private boolean isInternal = false;
+	private String shaderPackName;
+	private Path propertiesPath;
 
 	public IrisConfig() {
+		shaderPackName = null;
 		propertiesPath = FabricLoader.getInstance().getConfigDir().resolve("iris.properties");
-		try {
-			Files.createDirectories(FabricLoader.getInstance().getGameDir().resolve("shaderpacks"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		this.shaderpath = FabricLoader.getInstance().getGameDir().resolve("shaderpacks").resolve("internal");
 	}
 
 	/**
-	 * Deserializes and serializes the config
+	 * Initializes the configuration, loading it if it is present and creating a default config otherwise.
 	 * @throws IOException file exceptions
 	 */
-	public void createAndLoadProperties() throws IOException {
-		deserialize();
-		serialize();
-	}
-
-	/**
-	 * returns the path of the current shaderpack
-	 * @return the path of the shaderpack
-	 */
-	public Path getShaderPackPath(){
-		return shaderpath;
+	public void initialize() throws IOException {
+		load();
+		save();
 	}
 
 	/**
 	 * returns whether or not the current shaderpack is internal
 	 * @return if the shaderpack is internal
 	 */
-
 	public boolean isInternal(){
-		return isInternal;
+		return shaderPackName == null;
 	}
 
 	/**
-	 * Returns the name of the shaderpack
-	 * @return shaderpack name. If internal it returns "internal"
+	 * Returns the name of the current shaderpack
+	 * @return shaderpack name. If internal it returns "(internal)"
 	 */
-
 	public String getShaderPackName(){
-		if (isInternal){
-			return "internal";
+		if (shaderPackName == null){
+			return "(internal)";
 		}
-		return shaderpath.getFileName().toString();
+
+		return shaderPackName;
 	}
 
 	/**
@@ -75,32 +59,27 @@ public class IrisConfig {
 	 * @throws IOException if the file cannot be loaded
 	 */
 
-	public void deserialize() throws IOException {
+	public void load() throws IOException {
 		if (!Files.exists(propertiesPath)){
 			return;
 		}
+
 		Properties properties = new Properties();
 		properties.load(Files.newInputStream(propertiesPath));
-		this.shaderpath = Paths.get(properties.getProperty("shaderpack"));
+		shaderPackName = properties.getProperty("shaderPack");
+
+		if (shaderPackName != null && shaderPackName.equals("(internal)")) {
+			shaderPackName = null;
+		}
 	}
 
 	/**
-	 * Serializes the config into a file. Should be called after refreshing the values of entries
+	 * Serializes the config into a file. Should be called whenever any config values are modified.
 	 * @throws IOException file exceptions
 	 */
-
-	public void serialize() throws IOException {
+	public void save() throws IOException {
 		Properties properties = new Properties();
-		if (!Files.exists(shaderpath)){
-			if (!shaderpath.endsWith("internal")) {
-				Iris.logger.error(String.format("The specified shaderpack \"%s\" was not found! Change the value in iris.properties in your config directory! The system path should be \"%s\"", shaderpath.getFileName(), shaderpath));
-				Iris.logger.error("falling back to internal shaders...");
-			}
-			shaderpath = FabricLoader.getInstance().getModContainer("iris")
-					.orElseThrow(() -> new RuntimeException("Failed to get the mod container for Iris!")).getRootPath();
-			isInternal = true;
-		}
-		properties.setProperty("shaderpack", shaderpath.toString());
-		properties.store(Files.newOutputStream(propertiesPath), "This file stores configuration options for Iris, such as the currently active shaderpack");
+		properties.setProperty("shaderPack", getShaderPackName());
+		properties.store(Files.newOutputStream(propertiesPath), COMMENT);
 	}
 }
