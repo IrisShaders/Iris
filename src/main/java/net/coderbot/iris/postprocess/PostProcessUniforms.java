8c7a3c2bbca0fbec34fd67b99ad9d589d9c2da57
coderbot <coderbot16@gmail.com>
2020-11-27 14:21:17 -0800

    Start on postprocess / composite rendering things

---
+++ src/main/java/net/coderbot/iris/postprocess/PostProcessUniforms.java
@@ -0,0 +1,47 @@
+package net.coderbot.iris.postprocess;
+
+import net.coderbot.iris.gl.program.ProgramBuilder;
+import net.coderbot.iris.gl.uniform.UniformUpdateFrequency;
+
+public class PostProcessUniforms {
+	public static void addPostProcessUniforms(ProgramBuilder builder) {
+		// TODO: Some of these are shared uniforms
+
+		// Generic samplers
+		addSampler(builder, 0, "gcolor", "colortex0");
+		addSampler(builder, 1, "gdepth", "colortex1");
+		addSampler(builder, 2, "gnormal", "colortex2");
+		addSampler(builder, 3, "composite", "colortex3");
+		addSampler(builder, 7, "gaux1", "colortex4");
+		addSampler(builder, 8, "gaux2", "colortex5");
+		addSampler(builder, 9, "gaux3", "colortex6");
+		addSampler(builder, 10, "gaux4", "colortex7");
+
+		// Shadow
+		addSampler(builder, 4, "watershadow", "shadowtex0");
+
+		// Note: This will make it so that "watershadow" is printed twice to the log, oh well
+		// Check if the "watershadow" uniform is active. If so, the "shadow" texture will have a separate texture unit
+		boolean waterShadowEnabled = builder.location("watershadow").isPresent();
+
+		addSampler(builder, waterShadowEnabled ? 5 : 4, "shadow");
+
+		addSampler(builder, 5, "shadowtex1");
+		addSampler(builder, 13, "shadowcolor", "shadowcolor0");
+		addSampler(builder, 14, "shadowcolor1");
+
+		// Depth
+		addSampler(builder, 6, "gdepthtex", "depthtex0");
+		addSampler(builder, 11, "gdepthtex", "depthtex1");
+		addSampler(builder, 12, "gdepthtex", "depthtex2");
+
+		// Noise
+		addSampler(builder, 15, "noisetex");
+	}
+
+	private static void addSampler(ProgramBuilder builder, int textureUnit, String... names) {
+		for (String name : names) {
+			builder.uniform1i(UniformUpdateFrequency.ONCE, name, () -> textureUnit);
+		}
+	}
+}
