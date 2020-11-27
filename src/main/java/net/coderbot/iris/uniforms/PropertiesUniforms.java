604797e0949cfaf8c9a5fb3e7916479785043e66
coderbot <coderbot16@gmail.com>
2020-11-26 16:11:27 -0800

    Refactor PropertiesUniforms#getBlockEntityId and getEntityId

--- src/main/java/net/coderbot/iris/uniforms/PropertiesUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/PropertiesUniforms.java
@@ -1,13 +1,12 @@
 package net.coderbot.iris.uniforms;
 
-import net.coderbot.iris.Iris;
 import net.coderbot.iris.gl.program.ProgramBuilder;
 import net.coderbot.iris.gl.uniform.UniformUpdateFrequency;
 import net.coderbot.iris.shaderpack.IdMapParser;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
@@ -15,6 +14,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.IntSupplier;
 
 public final class PropertiesUniforms {
@@ -22,13 +22,16 @@ public final class PropertiesUniforms {
 	private PropertiesUniforms(){}
 
 	public static void addPropertiesUniforms(ProgramBuilder builder, IdMapParser idMap) {
+		Map<Identifier, Integer> blockIdMap = idMap.getBlockProperties();
+		Map<Identifier, Integer> entityIdMap = idMap.getEntityPropertiesMap();
+
 		builder
 		   .uniform1i(UniformUpdateFrequency.PER_FRAME, "heldItemId",
 				   new HeldItemSupplier(Hand.MAIN_HAND, idMap.getItemProperties()))
 		   .uniform1i(UniformUpdateFrequency.PER_FRAME, "heldItemId2",
 				   new HeldItemSupplier(Hand.OFF_HAND, idMap.getItemProperties()))
-		   .uniform1i(UniformUpdateFrequency.PER_FRAME, "blockEntityId", PropertiesUniforms::getBlockEntityId)
-		   .uniform1i(UniformUpdateFrequency.PER_FRAME, "entityId", PropertiesUniforms::getEntityId);
+		   .uniform1i(UniformUpdateFrequency.PER_FRAME, "blockEntityId", () -> getBlockEntityId(blockIdMap))
+		   .uniform1i(UniformUpdateFrequency.PER_FRAME, "entityId", () -> getEntityId(entityIdMap));
 
 	}
 
@@ -64,34 +67,40 @@ public final class PropertiesUniforms {
 	 * based on values from block.properties.
 	 * @return the blockentity id
 	 */
-	private static int getBlockEntityId() {
-		BlockEntity currentEntity = CapturedRenderingState.INSTANCE.getCurrentRenderedBlockEntity();
-		if (currentEntity != null){
+	private static int getBlockEntityId(Map<Identifier, Integer> blockIdMap) {
+		BlockEntity entity = CapturedRenderingState.INSTANCE.getCurrentRenderedBlockEntity();
 
-			Block block = MinecraftClient.getInstance().world.getBlockState(currentEntity.getPos()).getBlock();
-			boolean hasBlock = currentEntity.getType().supports(block) && currentEntity.hasWorld() && BlockEntityRenderDispatcher.INSTANCE.get(currentEntity) != null;
-			boolean isBlockParsed = Iris.getPipeline().getPack().getIdMapParser().getBlockProperties().containsKey(Registry.BLOCK.getId(block));
-			if (isBlockParsed && hasBlock){
-				return Iris.getPipeline().getPack().getIdMapParser().getBlockProperties().get(Registry.BLOCK.getId(block));
-			}
+		if (entity == null || !entity.hasWorld()) {
+			return -1;
 		}
-		return -1;
+
+		ClientWorld world = Objects.requireNonNull(MinecraftClient.getInstance().world);
+
+		Block blockAt = world.getBlockState(entity.getPos()).getBlock();
+
+		if (!entity.getType().supports(blockAt)) {
+			// Somehow the block here isn't compatible with the block entity at this location.
+			// I'm not sure how this could ever reasonably happen.
+			return -1;
+		}
+
+		return blockIdMap.getOrDefault(Registry.BLOCK.getId(blockAt), -1);
 	}
 
 	/**
 	 * returns the entity id based on the parsed entity id from entity.properties
 	 * @return the id the entity. Defaults to -1 if not specified
 	 */
-	private static int getEntityId() {
+	private static int getEntityId(Map<Identifier, Integer> entityIdMap) {
 		Entity entity = CapturedRenderingState.INSTANCE.getCurrentRenderedEntity();
-		if (entity != null){
-			Map<Identifier, Integer> entityMap = Iris.getPipeline().getPack().getIdMapParser().getEntityPropertiesMap();
-			Identifier entityId = Registry.ENTITY_TYPE.getId(entity.getType());
-			if (entityMap.containsKey(entityId)){
-				return entityMap.get(entityId); //if defined in entity.properties, return the value specified
-			}
-		}
-		return -1;//return -1 if the entity is not in entity.properties
-	}
 
+		if (entity == null) {
+			// Not valid if no entity is being rendered
+			return -1;
+		}
+
+		Identifier entityId = Registry.ENTITY_TYPE.getId(entity.getType());
+
+		return entityIdMap.getOrDefault(entityId, -1);
+	}
 }
