ea47dfb40d34aa8e8fff55698b8e14aecc3f8e5f
coderbot <coderbot16@gmail.com>
2020-11-29 12:08:16 -0800

    Very hacky approach to supporting centerDepth!

--- src/main/java/net/coderbot/iris/postprocess/CompositeRenderPasses.java
+++ src/main/java/net/coderbot/iris/postprocess/CompositeRenderPasses.java
@@ -10,7 +10,9 @@ import net.coderbot.iris.gl.program.Program;
 import net.coderbot.iris.gl.program.ProgramBuilder;
 import net.coderbot.iris.shaderpack.ShaderPack;
 import net.coderbot.iris.uniforms.CommonUniforms;
+import org.lwjgl.opengl.GL11C;
 import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL21C;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.Framebuffer;
@@ -54,8 +56,14 @@ public class CompositeRenderPasses {
 		}
 
 		RenderSystem.activeTexture(GL15.GL_TEXTURE0);
-
 		main.beginRead();
+
+		float[] depthValue = new float[1];
+		// Read a single pixel from the depth buffer
+		GL11C.glReadPixels(
+				main.textureWidth / 2, main.textureHeight / 2, 1, 1,
+				GL11C.GL_DEPTH_COMPONENT, GL11C.GL_FLOAT, depthValue
+		);
 
 		RenderSystem.activeTexture(GL15.GL_TEXTURE1);
 		RenderSystem.bindTexture(main.getDepthAttachment());
@@ -63,6 +71,7 @@ public class CompositeRenderPasses {
 
 		swap.beginWrite(false);
 		stages.get(0).use();
+		GL21C.glUniform1f(GL21C.glGetUniformLocation(stages.get(0).getProgramId(), "centerDepthSmooth"), depthValue[0]);
 		renderer.render();
 
 		main.beginWrite(false);
