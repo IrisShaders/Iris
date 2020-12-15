e8fd47d4688eee6a52f693c1c2cf38fed3cdb613
OverlordsIII <OverlordsIII@users.noreply.github.com>
2020-12-14 18:17:35 -0800

    fix import

--- src/main/java/net/coderbot/iris/Iris.java
+++ src/main/java/net/coderbot/iris/Iris.java
@@ -7,7 +7,6 @@ import net.coderbot.iris.shaderpack.ShaderPack;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 
