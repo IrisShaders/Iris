a96da0e1897a3751382a98891c80a01b507d66f0
OverlordsIII <OverlordsIII@users.noreply.github.com>
2020-12-23 17:56:31 -0800

    fix stuff

--- src/main/java/net/coderbot/iris/Iris.java
+++ src/main/java/net/coderbot/iris/Iris.java
@@ -3,6 +3,7 @@ package net.coderbot.iris;
 import com.google.common.base.Throwables;
 import net.coderbot.iris.config.IrisConfig;
 import net.coderbot.iris.pipeline.ShaderPipeline;
+import net.coderbot.iris.postprocess.CompositeRenderer;
 import net.coderbot.iris.shaderpack.ShaderPack;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
@@ -37,6 +38,7 @@ public class Iris implements ClientModInitializer {
 
 	private static ShaderPack currentPack;
 	private static ShaderPipeline pipeline;
+	private static CompositeRenderer compositeRenderer;
 	private static IrisConfig irisConfig;
 	public static KeyBinding reloadKeybind;
 
@@ -160,6 +162,14 @@ public class Iris implements ClientModInitializer {
 		}
 
 		return pipeline;
+	}
+
+	public static CompositeRenderer getCompositeRenderer() {
+		if (compositeRenderer == null) {
+			compositeRenderer = new CompositeRenderer(Objects.requireNonNull(currentPack));
+		}
+
+		return compositeRenderer;
 	}
 
 	public static ShaderPack getCurrentPack() {
