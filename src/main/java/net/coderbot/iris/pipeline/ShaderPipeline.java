bf198fa8194c2620df080fc1911b5f1ad4f41f3c
coderbot <coderbot16@gmail.com>
2020-12-31 14:14:48 -0800

    Fix at_tangent.w being assigned an invalid value, fixes reflections with Sildur's
    at_tangent.w should always be 1.0 or -1.0, 0.0 is completely invalid and breaks everything.

--- src/main/java/net/coderbot/iris/pipeline/ShaderPipeline.java
+++ src/main/java/net/coderbot/iris/pipeline/ShaderPipeline.java
@@ -173,7 +173,7 @@ public class ShaderPipeline {
 		// location assignment, so that might be something good to pursue in the future.
 		setupAttribute(pass, "mc_Entity", blockId, -1.0F, -1.0F, -1.0F);
 		setupAttribute(pass, "mc_midTexCoord", 0.0F, 0.0F, 0.0F, 0.0F);
-		setupAttribute(pass, "at_tangent", 1.0F, 0.0F, 0.0F, 0.0F);
+		setupAttribute(pass, "at_tangent", 1.0F, 0.0F, 0.0F, 1.0F);
 	}
 
 	private static void setupAttribute(Pass pass, String name, float v0, float v1, float v2, float v3) {
