b3ce6db45c68d8e8efabe80b74dbe87e5200dbeb
coderbot <coderbot16@gmail.com>
2020-12-31 23:03:51 -0800

    Continue wiring up program setup stuff in MultiPhaseParameters.Builder

--- src/main/java/net/coderbot/iris/mixin/renderlayer/MixinMultiPhaseParametersBuilder.java
+++ src/main/java/net/coderbot/iris/mixin/renderlayer/MixinMultiPhaseParametersBuilder.java
@@ -1,9 +1,14 @@
 package net.coderbot.iris.mixin.renderlayer;
 
 import net.coderbot.iris.layer.GbufferProgram;
+import net.coderbot.iris.layer.MultiPhaseParametersExtension;
 import net.coderbot.iris.layer.ProgramHolder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.render.RenderLayer;
 
@@ -19,5 +24,16 @@ public class MixinMultiPhaseParametersBuilder implements ProgramHolder {
 	@Override
 	public void program(GbufferProgram program) {
 		this.program = program;
+	}
+
+	@Inject(method = "build(Lnet/minecraft/client/render/RenderLayer$OutlineMode;)Lnet/minecraft/client/render/RenderLayer$MultiPhaseParameters;", at = @At("RETURN"))
+	private void iris$addProgramToBuiltParameters(@Coerce Object outlineMode, CallbackInfoReturnable<RenderLayer.MultiPhaseParameters> cir) {
+		if (this.program == null) {
+			return;
+		}
+
+		MultiPhaseParametersExtension extension = (MultiPhaseParametersExtension) (Object) cir.getReturnValue();
+
+		extension.useProgram(this.program);
 	}
 }
--- src/main/java/net/coderbot/iris/mixin/renderlayer/MixinRenderLayer.java
+++ src/main/java/net/coderbot/iris/mixin/renderlayer/MixinRenderLayer.java
@@ -5,6 +5,11 @@ import java.util.Optional;
 import net.coderbot.iris.layer.GbufferProgram;
 import net.coderbot.iris.layer.ProgramRenderLayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.client.render.RenderLayer;
 
@@ -14,9 +19,23 @@ import net.fabricmc.api.Environment;
 @Environment(EnvType.CLIENT)
 @Mixin(RenderLayer.class)
 public class MixinRenderLayer implements ProgramRenderLayer {
+	private static final String BUILD = "net/minecraft/client/render/RenderLayer$MultiPhaseParameters$Builder.build (Z)Lnet/minecraft/client/render/RenderLayer$MultiPhaseParameters;";
+
 	@Override
 	public Optional<GbufferProgram> getProgram() {
 		// By default, don't use shaders to render content
 		return Optional.empty();
+	}
+
+	@Inject(method = "<clinit>()V", at = @At(value = "INVOKE", target = BUILD), slice = @Slice(
+		from = @At(value = "CONSTANT", args = "stringValue=solid"),
+		to = @At(value = "FIELD", target = "net/minecraft/client/render/RenderLayer.CUTOUT:Lnet/minecraft/client/render/RenderLayer;")
+	), locals = LocalCapture.PRINT)
+	private static void addSolidShaders(CallbackInfo callback) {
+
+	}
+
+	static {
+		RenderLayer.getSolid();
 	}
 }
