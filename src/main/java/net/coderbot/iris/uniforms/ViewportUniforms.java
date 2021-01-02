22407f0708b6ff5363ef7319ce898a7991a4c072
coderbot <coderbot16@gmail.com>
2021-01-02 12:18:17 -0800

    Use the dimensions of the framebuffer in viewport uniforms

--- src/main/java/net/coderbot/iris/uniforms/ViewportUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/ViewportUniforms.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 import net.coderbot.iris.gl.uniform.UniformHolder;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.util.Window;
 
 /**
@@ -18,7 +19,7 @@ public final class ViewportUniforms {
 	/**
 	 * The currently open Minecraft window. The window object is final in MinecraftClient, so it's safe to cache it here.
 	 */
-	private static final Window WINDOW = Objects.requireNonNull(MinecraftClient.getInstance().getWindow());
+	private static final Framebuffer FRAMEBUFFER = Objects.requireNonNull(MinecraftClient.getInstance().getFramebuffer());
 
 	// cannot be constructed
 	private ViewportUniforms() {
@@ -30,9 +31,10 @@ public final class ViewportUniforms {
 	 * @param uniforms the program to make the uniforms available to
 	 */
 	public static void addViewportUniforms(UniformHolder uniforms) {
+		// TODO: What about the custom scale.composite3 property?
 		uniforms
-			.uniform1f(PER_FRAME, "viewHeight", WINDOW::getHeight)
-			.uniform1f(PER_FRAME, "viewWidth", WINDOW::getWidth)
+			.uniform1f(PER_FRAME, "viewHeight", () -> FRAMEBUFFER.textureHeight)
+			.uniform1f(PER_FRAME, "viewWidth", () -> FRAMEBUFFER.textureWidth)
 			.uniform1f(PER_FRAME, "aspectRatio", ViewportUniforms::getAspectRatio);
 	}
 
@@ -40,6 +42,6 @@ public final class ViewportUniforms {
 	 * @return the current viewport aspect ratio, calculated from the current Minecraft window size
 	 */
 	private static float getAspectRatio() {
-		return ((float) WINDOW.getWidth()) / ((float) WINDOW.getHeight());
+		return ((float) FRAMEBUFFER.textureWidth) / ((float) FRAMEBUFFER.textureHeight);
 	}
 }
