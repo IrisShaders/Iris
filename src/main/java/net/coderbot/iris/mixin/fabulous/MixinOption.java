42992421dd461e7c968b0684089cd82a63f7a902
coderbot <coderbot16@gmail.com>
2021-01-16 16:58:47 -0800

    Fix running outside of dev

--- src/main/java/net/coderbot/iris/mixin/fabulous/MixinOption.java
+++ src/main/java/net/coderbot/iris/mixin/fabulous/MixinOption.java
@@ -12,8 +12,7 @@ import net.fabricmc.api.Environment;
 @Environment(EnvType.CLIENT)
 @Mixin(Option.class)
 public class MixinOption {
-	@Redirect(method = "method_18554(Lnet/minecraft/client/options/GameOptions;Ljava/lang/Integer;)V", remap = false,
-		at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GlStateManager;supportsGl30()Z"))
+	@Redirect(method = "method_18554", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GlStateManager;supportsGl30()Z"))
 	private static boolean iris$onAttemptedToSelectFabulousGraphics() {
 		// Returning false here will cause Minecraft to cycle between Fancy and Fast, disabling Fabulous graphics
 		return false;
