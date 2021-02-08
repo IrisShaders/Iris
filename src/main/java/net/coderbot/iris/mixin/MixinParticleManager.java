8f2e700e49f800703a22a19b5549cbdf64f58200
coderbot <coderbot16@gmail.com>
2021-02-08 12:45:30 -0800

    Better tracking and sanity checks of the state of the program stack
    All pop operations now verify that they are popping the right program

--- src/main/java/net/coderbot/iris/mixin/MixinParticleManager.java
+++ src/main/java/net/coderbot/iris/mixin/MixinParticleManager.java
@@ -1,11 +1,13 @@
 package net.coderbot.iris.mixin;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 import net.coderbot.iris.layer.GbufferProgram;
 import net.coderbot.iris.layer.GbufferPrograms;
 import net.coderbot.iris.pipeline.ShaderPipeline;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -29,6 +31,9 @@ public class MixinParticleManager {
 	private static final String RENDER_PARTICLES = "renderParticles(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/render/LightmapTextureManager;Lnet/minecraft/client/render/Camera;F)V";
 	private static final String DRAW = "Lnet/minecraft/client/particle/ParticleTextureSheet;draw(Lnet/minecraft/client/render/Tessellator;)V";
 
+	@Unique
+	private ParticleTextureSheet lastSheet;
+
 	@Inject(method = RENDER_PARTICLES, at = @At("HEAD"))
 	private void iris$beginDrawingParticles(MatrixStack matrixStack, VertexConsumerProvider.Immediate immediate,
 											 LightmapTextureManager lightmapTextureManager, Camera camera, float f,
@@ -42,19 +47,26 @@ public class MixinParticleManager {
 										CallbackInfo ci, Iterator<ParticleTextureSheet> sheets, ParticleTextureSheet sheet,
 										Iterable<Particle> particles, Tessellator tessellator) {
 		GbufferPrograms.push(ShaderPipeline.getProgramForSheet(sheet));
+
+		if (lastSheet != null) {
+			throw new IllegalStateException("Particle rendering in weird state: lastSheet != null, lastSheet = " + lastSheet);
+		}
+
+		lastSheet = sheet;
 	}
 
 	@Inject(method = RENDER_PARTICLES, at = @At(value = "INVOKE", target = DRAW, shift = At.Shift.AFTER))
 	private void iris$postDrawParticleSheet(MatrixStack matrixStack, VertexConsumerProvider.Immediate immediate,
 											 LightmapTextureManager lightmapTextureManager, Camera camera, float f,
 											 CallbackInfo ci) {
-		GbufferPrograms.pop();
+		GbufferPrograms.pop(ShaderPipeline.getProgramForSheet(Objects.requireNonNull(lastSheet)));
+		lastSheet = null;
 	}
 
 	@Inject(method = RENDER_PARTICLES, at = @At("RETURN"))
 	private void iris$finishDrawingParticles(MatrixStack matrixStack, VertexConsumerProvider.Immediate immediate,
 										LightmapTextureManager lightmapTextureManager, Camera camera, float f,
 										CallbackInfo ci) {
-		GbufferPrograms.pop();
+		GbufferPrograms.pop(GbufferProgram.TEXTURED_LIT);
 	}
 }
