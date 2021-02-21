6f537f0d9a2c6641e517b085125a4098eae01590
coderbot <coderbot16@gmail.com>
2021-02-21 09:15:17 -0800

    Try to begin shadow model view

--- src/main/java/net/coderbot/iris/shadow/ShadowMatrices.java
+++ src/main/java/net/coderbot/iris/shadow/ShadowMatrices.java
@@ -1,5 +1,9 @@
 package net.coderbot.iris.shadow;
 
+import net.minecraft.client.util.math.Vector3f;
+import net.minecraft.util.math.Matrix4f;
+
+import java.nio.FloatBuffer;
 import java.util.Arrays;
 
 public class ShadowMatrices {
@@ -21,6 +25,13 @@ public class ShadowMatrices {
 		};
 	}
 
+	public static void createModelViewMatrix(Matrix4f target, float shadowAngle) {
+		target.loadIdentity();
+		target.multiply(Matrix4f.translate(0.0f, 0.0f, -100.0f));
+		target.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(90.0F));
+		target.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(90.0f + shadowAngle * -360.0f));
+	}
+
 	private static final class Tests {
 		public static void main(String[] args) {
 			float[] expected = new float[] {
@@ -31,6 +42,44 @@ public class ShadowMatrices {
 			};
 
 			test("ortho projection hpl=32", expected, createOrthoMatrix(32.0f));
+
+			float[] expectedModelViewAtDawn = new float[] {
+				// column 1
+				0.21545040607452393f,
+				5.820481518981069E-8f,
+				0.9765146970748901f,
+				0,
+				// column 2
+				-0.9765147466795349f,
+				1.2841844920785661E-8f,
+				0.21545039117336273f,
+				0,
+				// column 3
+				0,
+				-0.9999999403953552f,
+				5.960464477539063E-8f,
+				0,
+				// column 4
+				0,
+				0,
+				-100,
+				1,
+			};
+
+			Matrix4f modelView = new Matrix4f();
+			// shadow angle = 0 implies dawn
+			// This is odd, why is it not zero to get close? Need to go back and check the source numbers...
+			// 0.03455 ~= 12 degrees
+			createModelViewMatrix(modelView, 0.03455f);
+
+			test("model view at dawn", expectedModelViewAtDawn, toFloatArray(modelView));
+		}
+
+		private static float[] toFloatArray(Matrix4f matrix4f) {
+			FloatBuffer buffer = FloatBuffer.allocate(16);
+			matrix4f.writeToBuffer(buffer);
+
+			return buffer.array();
 		}
 
 		private static void test(String name, float[] expected, float[] created) {
@@ -38,7 +87,7 @@ public class ShadowMatrices {
 				System.err.println("test " + name + " failed: ");
 				System.err.println("    expected: ");
 				System.err.print(printMatrix(expected, 8));
-				System.err.println("    created: " + Arrays.toString(created));
+				System.err.println("    created: ");
 				System.err.print(printMatrix(created, 8));
 			} else {
 				System.out.println("test " + name + " passed");
