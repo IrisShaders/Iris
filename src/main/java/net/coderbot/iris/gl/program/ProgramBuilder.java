b9ce5606f7c6b27c73fac6536a95fa4112c17b6f
James R <71466513+ColdIce1605@users.noreply.github.com>
2021-03-21 19:02:10 -0500

    for you ims

--- src/main/java/net/coderbot/iris/gl/program/ProgramBuilder.java
+++ src/main/java/net/coderbot/iris/gl/program/ProgramBuilder.java
@@ -6,6 +6,7 @@ import net.coderbot.iris.gl.shader.ProgramCreator;
 import net.coderbot.iris.gl.shader.ShaderConstants;
 import net.coderbot.iris.gl.shader.ShaderType;
 import org.jetbrains.annotations.Nullable;
+import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL21C;
 
 public class ProgramBuilder extends ProgramUniforms.Builder {
@@ -34,8 +35,20 @@ public class ProgramBuilder extends ProgramUniforms.Builder {
 		} catch (RuntimeException e) {
 			throw new RuntimeException("Failed to compile vertex shader for program " + name, e);
 		}
+	if(geometrySource != null) {
+		try {
+<<<<<<< Updated upstream
+=======
+			geometry = new GlShader(ShaderType.GEOMETRY, name + ".gsh", geometrySource, EMPTY_CONSTANTS);
+		} catch (RuntimeException e) {
+			throw new RuntimeException("Failed to compile geometry shader for program " + name, e);
+		}
+	}	else {
+		geometry = null;
+	}
 
 		try {
+>>>>>>> Stashed changes
 			fragment = new GlShader(ShaderType.FRAGMENT, name + ".fsh", fragmentSource, EMPTY_CONSTANTS);
 		} catch (RuntimeException e) {
 			throw new RuntimeException("Failed to compile fragment shader for program " + name, e);
@@ -44,6 +57,10 @@ public class ProgramBuilder extends ProgramUniforms.Builder {
 		int programId = ProgramCreator.create(name, vertex, fragment);
 
 		vertex.destroy();
+<<<<<<< Updated upstream
+=======
+		if (geometry != null) geometry.destroy();
+>>>>>>> Stashed changes
 		fragment.destroy();
 
 		return new ProgramBuilder(name, programId);
