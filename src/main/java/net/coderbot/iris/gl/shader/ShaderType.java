158f913f9f20916053be1585c5d6cb79c1898210
James R <71466513+ColdIce1605@users.noreply.github.com>
2021-03-26 08:49:43 -0500

    Start work on compute

--- src/main/java/net/coderbot/iris/gl/shader/ShaderType.java
+++ src/main/java/net/coderbot/iris/gl/shader/ShaderType.java
@@ -4,11 +4,13 @@ package net.coderbot.iris.gl.shader;
 
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL32C;
+import org.lwjgl.opengl.GL45;
 
 /**
  * An enumeration over the supported OpenGL shader types.
  */
 public enum ShaderType {
+	COMPUTE(GL45.GL_COMPUTE_SHADER),
     VERTEX(GL20.GL_VERTEX_SHADER),
 	GEOMETRY(GL32C.GL_GEOMETRY_SHADER),
     FRAGMENT(GL20.GL_FRAGMENT_SHADER);
