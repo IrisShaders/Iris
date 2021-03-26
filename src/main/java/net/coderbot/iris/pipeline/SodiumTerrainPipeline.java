d773c161652293800d140bbf867334619e4fc1d2
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2021-03-26 12:07:40 -0500

    Fix internal shaders issue in SodiumTerrainPipeline

--- src/main/java/net/coderbot/iris/pipeline/SodiumTerrainPipeline.java
+++ src/main/java/net/coderbot/iris/pipeline/SodiumTerrainPipeline.java
@@ -11,6 +11,7 @@ import net.coderbot.iris.shaderpack.transform.BuiltinUniformReplacementTransform
 import net.coderbot.iris.shaderpack.transform.StringTransformations;
 import net.coderbot.iris.shaderpack.transform.Transformations;
 import net.coderbot.iris.uniforms.CommonUniforms;
+import net.coderbot.iris.uniforms.SamplerUniforms;
 import net.coderbot.iris.uniforms.builtin.BuiltinReplacementUniforms;
 
 public class SodiumTerrainPipeline {
@@ -120,6 +121,8 @@ public class SodiumTerrainPipeline {
 		ProgramUniforms.Builder uniforms = ProgramUniforms.builder("<sodium shaders>", programId);
 
 		CommonUniforms.addCommonUniforms(uniforms, programSet.getPack().getIdMap());
+		SamplerUniforms.addWorldSamplerUniforms(uniforms);
+		SamplerUniforms.addDepthSamplerUniforms(uniforms);
 		BuiltinReplacementUniforms.addBuiltinReplacementUniforms(uniforms);
 
 		return uniforms.buildUniforms();
