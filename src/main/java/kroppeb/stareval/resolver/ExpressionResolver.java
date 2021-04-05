557d019f55b40a61c34eac44a46452abc1aac752
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2021-04-05 23:26:43 +0200

    Few more debug lines

--- src/main/java/kroppeb/stareval/resolver/ExpressionResolver.java
+++ src/main/java/kroppeb/stareval/resolver/ExpressionResolver.java
@@ -113,10 +113,10 @@ functions:
 		}
 		
 		result = resolveCallExpressionInternal(targetType, name, inner, true);
-		if(result != null){
+		if (result != null) {
 			log("[DEBUG] resolved function %s with args %s to type %s using implicit inner casts",
 					name, inner, targetType);
-		}else{
+		} else {
 			log("[DEBUG] failed to resolve function %s with args %s to type %s",
 					name, inner, targetType);
 		}
@@ -192,12 +192,21 @@ functions:
 			Type type = this.variableTypeMap.apply(name);
 			if (type == null)
 				throw new RuntimeException("Unknown variable: " + name);
-			if (type.equals(targetType))
-				return (c, r) -> c.getVariable(name).evaluateTo(c, r);
-			if (!allowImplicit)
+			if (type.equals(targetType)){
+				log("[DEBUG] resolved variable %s to type %s", name, targetType);
+				return new Expression() {
+					@Override
+					public void evaluateTo(FunctionContext c, FunctionReturn r) {
+						c.getVariable(name).evaluateTo(c, r);
+					}
+				};
+			}
+			if (!allowImplicit) {
+				log("[DEBUG] failed to resolve variable %s (of type %s) to type %s without implicit casts",
+						name, type, targetType);
 				return null;
+			}
 			castable = (c, r) -> c.getVariable(name).evaluateTo(c, r);
-			;
 			innerType = type;
 		} else {
 			throw new RuntimeException("unexpected token: " + expression.toString());
@@ -207,9 +216,12 @@ functions:
 		
 		for (TypedFunction f : casts) {
 			if (f.getParameterTypes()[0].equals(innerType)) {
+				log("[DEBUG] resolved %s to type %s using implicit casts",
+						expression, targetType);
 				return new CallExpression(f, new Expression[]{castable});
 			}
 		}
+		log("[DEBUG] failed to resolved %s to type %s, even using implicit casts");
 		return null;
 	}
 	
