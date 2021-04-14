efa4e0cab2ae046dd69bdc5929e4a27c1cb9c1ba
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2021-04-14 22:54:16 +0200

    StringReader documentation

--- src/main/java/kroppeb/stareval/parser/StringReader.java
+++ src/main/java/kroppeb/stareval/parser/StringReader.java
@@ -1,67 +1,113 @@
 package kroppeb.stareval.parser;
 
+/**
+ * A class to facilitate the reading of strings, will completely ignore all spaces.
+ */
 public class StringReader {
-	final String string;
-	int index = 0;
-	int lastIndex = 0;
-	int nextIndex = 0;
-	int mark = 0;
-	
-	
+	/**
+	 * The string we are reading
+	 */
+	private final String string;
+	/**
+	 * The next index to read at
+	 */
+	private int nextIndex = -1;
+	/**
+	 * The last index read
+	 */
+	private int lastIndex;
+	/**
+	 * The start of {@link #substring()}
+	 */
+	private int mark;
+
+
 	public StringReader(String string) {
 		this.string = string;
-		this.skipWhiteSpace();
+		this.move();
+
+		// Initializing these variables to the actual start of the string.
+		this.lastIndex = nextIndex;
+		this.mark();
 	}
-	
-	private void skipWhiteSpace() {
-		this.lastIndex = this.index;
-		this.index = this.nextIndex;
-		if (index >= this.string.length())
+
+	/**
+	 * moves the indexes, nextIndex will skip all spaces.
+	 */
+	private void move() {
+		this.lastIndex = this.nextIndex;
+		if (nextIndex >= this.string.length())
 			return;
 		this.nextIndex++;
 		while (this.nextIndex < this.string.length() && this.string.charAt(this.nextIndex) == ' ') {
 			nextIndex++;
 		}
 	}
-	
+
+	/**
+	 * @return The character that would be returned by the next call to {@link #read}
+	 */
 	public char peek() {
-		return string.charAt(this.index);
+		return string.charAt(this.nextIndex);
 	}
-	
+
+	/**
+	 * Skips the current character
+	 */
 	public void skip() {
-		this.skipWhiteSpace();
+		this.move();
 	}
-	
+
+	/**
+	 * Read a character
+	 */
 	public char read() {
 		char current = peek();
 		skip();
 		return current;
 	}
-	
+
+	/**
+	 * Read a character and verify it's the expected character.
+	 */
 	public void read(char c) throws Exception {
 		char read = read();
-		if(read != c)
+		if (read != c)
 			throw new Exception("unexpected character: '" + read + "' expected '" + c + "'");
 	}
-	
-	public boolean tryRead(char c){
+
+	/**
+	 * Try to read a character.
+	 *
+	 * @param c the character to read
+	 * @return whether it could read the character.
+	 */
+	public boolean tryRead(char c) {
 		char read = peek();
-		if(read != c)
+		if (read != c)
 			return false;
 		skip();
 		return true;
 	}
-	
+
+	/**
+	 * Place a mark at the character that just got read. Used for {@link #substring()}
+	 */
 	public void mark() {
 		this.mark = this.lastIndex;
 	}
-	
-	
+
+	/**
+	 * @return a string, starting at the last call to {@link #mark()}, up to and including the last read character
+	 */
 	public String substring() {
 		return this.string.substring(this.mark, this.lastIndex + 1);
 	}
-	
+
+	/**
+	 * @return whether there is more text to read.
+	 */
 	public boolean canRead() {
-		return this.index < this.string.length();
+		return this.nextIndex < this.string.length();
 	}
 }
