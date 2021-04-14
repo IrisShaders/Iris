995c4d78b62e99640893d52a6534d25216766efe
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2021-04-14 21:05:19 +0200

    Fix unary operators having higher precedence than calling

--- src/main/java/kroppeb/stareval/parser/TokenStack.java
+++ src/main/java/kroppeb/stareval/parser/TokenStack.java
@@ -39,10 +39,10 @@ class TokenStack {
 	 * Executes following reduce steps:
 	 * <ul>
 	 *     <li>{@link IdToken} | {@link ArgsToken} => {@link CallToken}</li>
-	 *     <li>{@link UnaryOperatorToken} | {@link ExpressionToken} => {@link UnaryExpressionToken}</li>
+	 *     <li>{@link UnaryOperatorToken}, {@link ExpressionToken} | {@link BinaryOperatorToken} => {@link UnaryExpressionToken} | {@link BinaryOperatorToken}</li>
 	 *     <li>
-	 *         {@link ExpressionToken}, {@link BinaryOperatorToken}, {@link ExpressionToken} | {@link BinaryOperatorToken}
-	 *         where the BinOp on the stack has a higher or equal priority to the one being added, the 3 items on the
+	 *         {@link ExpressionToken}, {@link BinaryOperatorToken}, {@link ExpressionToken} | {@link BinaryOperatorToken} <br/>
+	 *         where the operator on the stack has a higher or equal priority to the one being added, the 3 items on the
 	 *         stack get popped, merged to a {@link BinaryExpressionToken} and placed on the stack.
 	 *         The new token is then pushed again.
 	 *     </li>
@@ -51,34 +51,37 @@ class TokenStack {
 	void push(Token token) throws Exception {
 		// in Kotlin I'd mark this tailrecursive.
 		Token top = this.peek();
-		if (token instanceof ExpressionToken) {
-			if (token instanceof ArgsToken && top instanceof IdToken) {
-				this.pop();
-				push(new CallToken(((IdToken) top).id, ((ArgsToken) token).tokens));
-				return;
-			}
-			else if (top instanceof UnaryOperatorToken) {
-				this.pop();
-				push(new UnaryExpressionToken(((UnaryOperatorToken) top).op, (ExpressionToken) token));
-				return;
-			}
-		}
-		else if (token instanceof BinaryOperatorToken) {
+		if (token instanceof ArgsToken && top instanceof IdToken) {
+			this.pop();
+			push(new CallToken(((IdToken) top).id, ((ArgsToken) token).tokens));
+			return;
+		} else if (token instanceof BinaryOperatorToken) {
 			// bin ops need to follow an expression
 			assert top instanceof ExpressionToken;
 
 
-			if (stack.size() >= 3) {
+			if (stack.size() >= 2) {
 				Token other = this.peek(1);
 				if (other instanceof BinaryOperatorToken &&
-					((BinaryOperatorToken) other).op.priority <= ((BinaryOperatorToken) token).op.priority) {
+						((BinaryOperatorToken) other).op.priority <= ((BinaryOperatorToken) token).op.priority) {
 					// stack[ 'a', '*', 'b'], token = '+'
 					ExpressionToken b = (ExpressionToken) this.pop();
 					this.pop();
+					// each binop token requires an ExpressionToken on the stack (could merge them instead?)
 					ExpressionToken a = (ExpressionToken) this.pop();
 
 					// merge op
 					this.stack.add(new BinaryExpressionToken(((BinaryOperatorToken) other).op, a, b));
+					// retry pushing this token
+					this.push(token);
+					return;
+				} else if (other instanceof UnaryOperatorToken) {
+					// stack['-', 'b'], token = '*'
+					ExpressionToken b = (ExpressionToken) this.pop();
+					this.pop();
+
+					// merge op
+					this.stack.add(new UnaryExpressionToken(((UnaryOperatorToken) other).op, b));
 					// retry pushing this token
 					this.push(token);
 					return;
@@ -91,16 +94,17 @@ class TokenStack {
 
 	/**
 	 * Pops an expression after trying to reduce the stack.
-	 * Executes following reduce step:
+	 * Executes following reduce steps:
 	 * <ul>
+	 *     <li>{@link UnaryOperatorToken}, {@link ExpressionToken} => {@link BinaryExpressionToken}</li>
 	 *     <li>{@link ExpressionToken}, {@link BinaryOperatorToken}, {@link ExpressionToken} => {@link BinaryExpressionToken}</li>
 	 * </ul>
 	 */
 	ExpressionToken expressionReducePop() throws Exception {
-		ExpressionToken token = (ExpressionToken)pop();
-		while (stack.size() >= 2) {
+		ExpressionToken token = (ExpressionToken) pop();
+		while (stack.size() >= 1) {
 			Token x = peek(0);
-			Token a = peek(1);
+			Token a = peek(1); // can be null
 
 
 			if (x instanceof BinaryOperatorToken) {
@@ -109,6 +113,9 @@ class TokenStack {
 
 				pop(2);
 				token = new BinaryExpressionToken(((BinaryOperatorToken) x).op, (ExpressionToken) a, token);
+			} else if (x instanceof UnaryOperatorToken) {
+				pop(1);
+				token = new UnaryExpressionToken(((UnaryOperatorToken) x).op, token);
 			} else {
 				break;
 			}
