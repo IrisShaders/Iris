70e4d7bd33a3e037c0565552cabed44890fcb0e0
coderbot <coderbot16@gmail.com>
2021-05-17 10:35:05 -0700

    Attempt to properly parse vec4 directives

--- src/main/java/net/coderbot/iris/shaderpack/DispatchingDirectiveHolder.java
+++ src/main/java/net/coderbot/iris/shaderpack/DispatchingDirectiveHolder.java
@@ -15,12 +15,14 @@ public class DispatchingDirectiveHolder implements DirectiveHolder {
 	private final Map<String, Consumer<String>> stringConstVariables;
 	private final Map<String, IntConsumer> intConstVariables;
 	private final Map<String, FloatConsumer> floatConstVariables;
+	private final Map<String, Consumer<Vector4f>> vec4ConstVariables;
 
 	public DispatchingDirectiveHolder() {
 		booleanConstVariables = new HashMap<>();
 		stringConstVariables = new HashMap<>();
 		intConstVariables = new HashMap<>();
 		floatConstVariables = new HashMap<>();
+		vec4ConstVariables = new HashMap<>();
 	}
 
 	public void processSource(String source) {
@@ -52,6 +54,7 @@ public class DispatchingDirectiveHolder implements DirectiveHolder {
 			typeCheckHelper("int", intConstVariables, directive);
 			typeCheckHelper("int", stringConstVariables, directive);
 			typeCheckHelper("float", floatConstVariables, directive);
+			typeCheckHelper("vec4", vec4ConstVariables, directive);
 		} else if (type == ConstDirectiveParser.Type.INT) {
 			// GLSL does not actually have a string type, so string constant directives use "const int" instead.
 			Consumer<String> stringConsumer = stringConstVariables.get(key);
@@ -78,6 +81,7 @@ public class DispatchingDirectiveHolder implements DirectiveHolder {
 
 			typeCheckHelper("bool", booleanConstVariables, directive);
 			typeCheckHelper("float", floatConstVariables, directive);
+			typeCheckHelper("vec4", vec4ConstVariables, directive);
 		} else if (type == ConstDirectiveParser.Type.FLOAT) {
 			FloatConsumer consumer = floatConstVariables.get(key);
 
@@ -96,8 +100,52 @@ public class DispatchingDirectiveHolder implements DirectiveHolder {
 			typeCheckHelper("bool", booleanConstVariables, directive);
 			typeCheckHelper("int", intConstVariables, directive);
 			typeCheckHelper("int", stringConstVariables, directive);
+			typeCheckHelper("vec4", vec4ConstVariables, directive);
 		} else if (type == ConstDirectiveParser.Type.VEC4) {
-			Iris.logger.warn("Ignoring " + directive + " because vec4 directives are currently unsupported.");
+			Consumer<Vector4f> consumer = vec4ConstVariables.get(key);
+
+			if (consumer != null) {
+				if (!value.startsWith("vec4")) {
+					Iris.logger.error("Failed to process " + directive + ": value was not a valid vec4 constructor");
+				}
+
+				String vec4Args = value.substring("vec4".length()).trim();
+
+				if (!vec4Args.startsWith("(") || !vec4Args.endsWith(")")) {
+					Iris.logger.error("Failed to process " + directive + ": value was not a valid vec4 constructor");
+				}
+
+				vec4Args = vec4Args.substring(1, vec4Args.length() - 1);
+
+				String[] parts = vec4Args.split(",");
+
+				for (int i = 0; i < parts.length; i++) {
+					parts[i] = parts[i].trim();
+				}
+
+				if (parts.length != 4) {
+					Iris.logger.error("Failed to process " + directive +
+						": expected 4 arguments to a vec4 constructor, got " + parts.length);
+				}
+
+				try {
+					consumer.accept(new Vector4f(
+						Float.parseFloat(parts[0]),
+						Float.parseFloat(parts[1]),
+						Float.parseFloat(parts[2]),
+						Float.parseFloat(parts[3])
+					));
+				} catch (NumberFormatException e) {
+					Iris.logger.error("Failed to process " + directive, e);
+				}
+
+				return;
+			}
+
+			typeCheckHelper("bool", booleanConstVariables, directive);
+			typeCheckHelper("int", intConstVariables, directive);
+			typeCheckHelper("int", stringConstVariables, directive);
+			typeCheckHelper("float", floatConstVariables, directive);
 		}
 	}
 
@@ -153,7 +201,6 @@ public class DispatchingDirectiveHolder implements DirectiveHolder {
 
 	@Override
 	public void acceptConstVec4Directive(String name, Consumer<Vector4f> consumer) {
-		// TODO
-		Iris.logger.warn("Not looking for a const vec4 directive with the name " + name + " since this type of directive is not currently supported.");
+		vec4ConstVariables.put(name, consumer);
 	}
 }
