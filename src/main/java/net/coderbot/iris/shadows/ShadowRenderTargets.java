e91f02614909275b815b8f31da0face99ace9d29
coderbot <coderbot16@gmail.com>
2021-05-22 19:20:10 -0700

    Un-break colored shadows

--- src/main/java/net/coderbot/iris/shadows/ShadowRenderTargets.java
+++ src/main/java/net/coderbot/iris/shadows/ShadowRenderTargets.java
@@ -7,6 +7,7 @@ import net.coderbot.iris.gl.texture.PixelFormat;
 import net.coderbot.iris.gl.texture.PixelType;
 import net.coderbot.iris.rendertarget.DepthTexture;
 import org.lwjgl.opengl.GL11C;
+import org.lwjgl.opengl.GL13C;
 import org.lwjgl.opengl.GL20C;
 
 import java.nio.ByteBuffer;
@@ -49,12 +50,18 @@ public class ShadowRenderTargets {
 
 			GL11C.glTexImage2D(GL11C.GL_TEXTURE_2D, 0, format.getGlFormat(), resolution, resolution, 0,
 					PixelFormat.RGBA.getGlFormat(), PixelType.UNSIGNED_BYTE.getGlFormat(), NULL_BUFFER);
+			GL11C.glTexParameteri(GL11C.GL_TEXTURE_2D, GL11C.GL_TEXTURE_MIN_FILTER, GL11C.GL_LINEAR);
+			GL11C.glTexParameteri(GL11C.GL_TEXTURE_2D, GL11C.GL_TEXTURE_MAG_FILTER, GL11C.GL_LINEAR);
+			GL11C.glTexParameteri(GL11C.GL_TEXTURE_2D, GL11C.GL_TEXTURE_WRAP_S, GL13C.GL_CLAMP_TO_BORDER);
+			GL11C.glTexParameteri(GL11C.GL_TEXTURE_2D, GL11C.GL_TEXTURE_WRAP_T, GL13C.GL_CLAMP_TO_BORDER);
 
 			framebuffer.addColorAttachment(i, targets[i]);
 			drawBuffers[i] = i;
 		}
 
 		framebuffer.drawBuffers(drawBuffers);
+
+		RenderSystem.bindTexture(0);
 	}
 
 	public GlFramebuffer getFramebuffer() {
