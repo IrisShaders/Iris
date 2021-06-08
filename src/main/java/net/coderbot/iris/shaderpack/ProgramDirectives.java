88805c3a2537dea1cb18f2c553b29ad98f37693d
coderbot <coderbot16@gmail.com>
2021-06-07 20:26:03 -0700

    Revert the drawbuffer default changes due to issues with Sildur's Vibrant Shaders on 1.17

--- src/main/java/net/coderbot/iris/shaderpack/ProgramDirectives.java
+++ src/main/java/net/coderbot/iris/shaderpack/ProgramDirectives.java
@@ -22,9 +22,13 @@ public class ProgramDirectives {
 
 	ProgramDirectives(ProgramSource source, ShaderProperties properties, Set<Integer> supportedRenderTargets) {
 		// DRAWBUFFERS is only detected in the fragment shader source code (.fsh).
-		// If there's no explicit declaration, then by default /* DRAWBUFFERS:01234567 */ is inferred.
-		// This behavior is needed for SEUS v08 and SEUS v10 to work, otherwise they will break badly.
-		drawBuffers = findDrawbuffersDirective(source.getFragmentSource()).orElse(new int[] { 0, 1, 2, 3, 4, 5, 6, 7 });
+		// If there's no explicit declaration, then by default /* DRAWBUFFERS:0 */ is inferred.
+		// For SEUS v08 and SEUS v10 to work, this will need to be set to 01234567. However, doing this causes
+		// TAA to break on Sildur's Vibrant Shaders, since gbuffers_skybasic lacks a DRAWBUFFERS directive, causing
+		// undefined data to be written to colortex7.
+		//
+		// TODO: Figure out how to infer the DRAWBUFFERS directive when it is missing.
+		drawBuffers = findDrawbuffersDirective(source.getFragmentSource()).orElse(new int[] { 0 });
 
 		if (properties != null) {
 			viewportScale = properties.getViewportScaleOverrides().getOrDefault(source.getName(), 1.0f);
