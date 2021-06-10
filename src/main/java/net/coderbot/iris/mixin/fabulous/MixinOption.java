bc7f2b0c167a4fd34bd83304879f149c90006d5c
IMS212 <31803019+IMS212@users.noreply.github.com>
2021-06-09 22:26:13 -0700

    Allow using fabulous when shaders are not enabled

--- src/main/java/net/coderbot/iris/mixin/fabulous/MixinOption.java
+++ src/main/java/net/coderbot/iris/mixin/fabulous/MixinOption.java
@@ -1,5 +1,6 @@
 package net.coderbot.iris.mixin.fabulous;
 
+import net.coderbot.iris.Iris;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -15,6 +16,6 @@ public class MixinOption {
 	@Redirect(method = "method_18554", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GlStateManager;supportsGl30()Z"))
 	private static boolean iris$onAttemptedToSelectFabulousGraphics() {
 		// Returning false here will cause Minecraft to cycle between Fancy and Fast, disabling Fabulous graphics
-		return false;
+		return !Iris.getIrisConfig().areShadersEnabled();
 	}
 }
