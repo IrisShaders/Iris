380f5a2b9b11829c101472f7f75fb66610faa7e1
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2021-07-15 22:37:11 +0200

    Fixed vertex ordering

--- src/main/java/net/coderbot/iris/pipeline/newshader/TriforcePatcher.java
+++ src/main/java/net/coderbot/iris/pipeline/newshader/TriforcePatcher.java
@@ -221,10 +221,11 @@ public class TriforcePatcher {
 					"vec2 _iris_internal_screenSize = vec2(viewHeight, viewWidth);\n" +
 					"vec2 _iris_internal_lineDir = normalize((_iris_internal_ndcEnd.xy - _iris_internal_ndcStart.xy) * _iris_internal_screenSize);\n" +
 					"vec2 _iris_internal_lineOffset = (vec2(-_iris_internal_lineDir.y, _iris_internal_lineDir.x) * _iris_internal_line_width) / _iris_internal_screenSize;\n" +
+					"if(_iris_internal_lineOffset.x < 0) { _iris_internal_lineOffset = _iris_internal_lineOffset * -1; }\n" +
 					"if(gl_VertexID % 2 == 0) {\n" +
-					"_iris_internal_ndcStart = _iris_internal_ndcStart - vec3(_iris_internal_lineOffset, 0.0);\n" +
-					"} else {\n"+
 					"_iris_internal_ndcStart = _iris_internal_ndcStart + vec3(_iris_internal_lineOffset, 0.0);\n" +
+					"} else {\n"+
+					"_iris_internal_ndcStart = _iris_internal_ndcStart - vec3(_iris_internal_lineOffset, 0.0);\n" +
 					"}\n" +
 					"vec4 _iris_internal_recPos = inverse(gl_ModelViewProjectionMatrix) * vec4(_iris_internal_ndcStart, 1.0);\n" +
 					"Position = _iris_internal_recPos.xyz / _iris_internal_recPos.w;\n" +
