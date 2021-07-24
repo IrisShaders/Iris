69f9595926932eb52e9dcf93e7d83c4783b3b87f
coderbot <coderbot16@gmail.com>
2021-07-23 23:51:01 -0700

    Ensure that the wrapped variants of glint layers are buffered too (Fixes #618)

--- src/main/java/net/coderbot/iris/mixin/fantastic/MixinBufferBuilderStorage.java
+++ src/main/java/net/coderbot/iris/mixin/fantastic/MixinBufferBuilderStorage.java
@@ -1,6 +1,8 @@
 package net.coderbot.iris.mixin.fantastic;
 
 import net.coderbot.iris.fantastic.ExtendedBufferStorage;
+import net.coderbot.iris.layer.EntityColorRenderPhase;
+import net.coderbot.iris.layer.EntityColorWrappedRenderLayer;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.RenderLayer;
@@ -13,6 +15,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.SortedMap;
 
 @Mixin(BufferBuilderStorage.class)
@@ -37,6 +41,19 @@ public class MixinBufferBuilderStorage implements ExtendedBufferStorage {
 
 		// Similar deal with wool on sheeps.
 		iris$assignBufferBuilder(entityBuilders, RenderLayer.getEntityCutoutNoCull(new Identifier("textures/entity/sheep/sheep_fur.png")));
+
+		// Vanilla depends on being able to write to some buffers at the same time as other ones
+		// This includes enchantment glints.
+		//
+		// We need to make sure that wrapped variants of buffered render layers are buffered too,
+		// or else we'll get crashes with this approach.
+		List<RenderLayer> existingLayers = new ArrayList<>(entityBuilders.keySet());
+		EntityColorRenderPhase entityColorPhase = new EntityColorRenderPhase(true, 0.0F);
+
+		for (RenderLayer existingLayer : existingLayers) {
+			RenderLayer wrappedLayer = new EntityColorWrappedRenderLayer("iris_entity_color", existingLayer, entityColorPhase);
+			iris$assignBufferBuilder(entityBuilders, wrappedLayer);
+		}
 	}
 
 	@Unique
