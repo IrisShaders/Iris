510c3d4244dd47590139f018ce1e352cda1615e8
IMS212 <31803019+IMS212@users.noreply.github.com>
2021-08-19 14:58:11 -0700

    addIrisExclusiveUniforms

--- src/main/java/net/coderbot/iris/uniforms/CommonUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/CommonUniforms.java
@@ -50,7 +50,7 @@ public final class CommonUniforms {
 		SystemTimeUniforms.addSystemTimeUniforms(uniforms);
 		new CelestialUniforms(directives.getSunPathRotation()).addCelestialUniforms(uniforms);
 		IdMapUniforms.addIdMapUniforms(uniforms, idMap);
-		IrisExclusiveUniforms.addIrisUniforms(uniforms);
+		IrisExclusiveUniforms.addIrisExclusiveUniforms(uniforms);
 		MatrixUniforms.addMatrixUniforms(uniforms, directives);
 		HardcodedCustomUniforms.addHardcodedCustomUniforms(uniforms, updateNotifier);
 		FogUniforms.addFogUniforms(uniforms);
--- src/main/java/net/coderbot/iris/uniforms/IrisExclusiveUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/IrisExclusiveUniforms.java
@@ -5,7 +5,7 @@ import net.coderbot.iris.gl.uniform.UniformHolder;
 import static net.coderbot.iris.gl.uniform.UniformUpdateFrequency.PER_TICK;
 
 public class IrisExclusiveUniforms {
-	public static void addIrisUniforms(UniformHolder uniforms) {
+	public static void addIrisExclusiveUniforms(UniformHolder uniforms) {
 		uniforms
 			.uniform1f(PER_TICK, "dragonDeathProgress", IrisExclusiveUniforms::getDragonDeathProgress);
 	}
