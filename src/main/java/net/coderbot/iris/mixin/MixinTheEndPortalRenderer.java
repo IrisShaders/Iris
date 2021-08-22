c62c99c1e1866836d30f75a557a9fc138635df9e
IMS212 <31803019+IMS212@users.noreply.github.com>
2021-08-21 20:36:56 -0700

    Merge end portal change

--- src/main/java/net/coderbot/iris/mixin/MixinTheEndPortalRenderer.java
+++ src/main/java/net/coderbot/iris/mixin/MixinTheEndPortalRenderer.java
@@ -1,16 +1,16 @@
 package net.coderbot.iris.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Matrix3f;
+import com.mojang.math.Matrix4f;
 import net.coderbot.iris.Iris;
 import net.coderbot.iris.uniforms.SystemTimeUniforms;
-import net.minecraft.block.entity.EndPortalBlockEntity;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.block.entity.EndPortalBlockEntityRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Matrix3f;
-import net.minecraft.util.math.Matrix4f;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.TheEndPortalRenderer;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.entity.TheEndPortalBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,8 +18,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(EndPortalBlockEntityRenderer.class)
-public class MixinEndPortalBlockEntityRenderer {
+@Mixin(TheEndPortalRenderer.class)
+public class MixinTheEndPortalRenderer {
 	@Unique
 	private static final float RED = 0.075f;
 
@@ -31,13 +31,12 @@ public class MixinEndPortalBlockEntityRenderer {
 
 	// getHeight
 	@Shadow
-	protected float method_3594() {
+	protected float getOffset() {
 		return 0.75F;
 	}
 
 	@Inject(method = "render", at = @At("HEAD"), cancellable = true)
-	public void iris$onRender(EndPortalBlockEntity entity, float tickDelta, MatrixStack matrices,
-							  VertexConsumerProvider vertexConsumers, int light, int overlay, CallbackInfo ci) {
+	public void iris$onRender(TheEndPortalBlockEntity entity, float tickDelta, PoseStack poseStack, MultiBufferSource multiBufferSource, int light, int overlay, CallbackInfo ci) {
 		if (!Iris.getCurrentPack().isPresent()) {
 			return;
 		}
@@ -46,15 +45,15 @@ public class MixinEndPortalBlockEntityRenderer {
 
 		// POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL
 		VertexConsumer vertexConsumer =
-				vertexConsumers.getBuffer(RenderLayer.getEntitySolid(EndPortalBlockEntityRenderer.PORTAL_TEXTURE));
+				multiBufferSource.getBuffer(RenderType.entitySolid(TheEndPortalRenderer.END_PORTAL_LOCATION));
 
-		Matrix4f pose = matrices.peek().getModel();
-		Matrix3f normal = matrices.peek().getNormal();
+		Matrix4f pose = poseStack.last().pose();
+		Matrix3f normal = poseStack.last().normal();
 
 		// animation with a period of 100 seconds.
 		// note that texture coordinates are wrapping, not clamping.
 		float progress = (SystemTimeUniforms.TIMER.getFrameTimeCounter() * 0.01f) % 1f;
-		float height = method_3594();
+		float height = getOffset();
 
 		quad(entity, vertexConsumer, pose, normal, Direction.UP, progress, overlay, light,
 				0.0f, height, 1.0f,
@@ -94,34 +93,34 @@ public class MixinEndPortalBlockEntityRenderer {
 	}
 
 	@Unique
-	private void quad(EndPortalBlockEntity entity, VertexConsumer vertexConsumer, Matrix4f pose, Matrix3f normal,
+	private void quad(TheEndPortalBlockEntity entity, VertexConsumer vertexConsumer, Matrix4f pose, Matrix3f normal,
 					  Direction direction, float progress, int overlay, int light,
 					  float x1, float y1, float z1,
 					  float x2, float y2, float z2,
 					  float x3, float y3, float z3,
 					  float x4,float y4, float z4) {
-		if (!entity.shouldDrawSide(direction)) {
+		if (!entity.shouldRenderFace(direction)) {
 			return;
 		}
 
-		float nx = direction.getOffsetX();
-		float ny = direction.getOffsetY();
-		float nz = direction.getOffsetZ();
+		float nx = direction.getStepX();
+		float ny = direction.getStepY();
+		float nz = direction.getStepZ();
 
 		vertexConsumer.vertex(pose, x1, y1, z1).color(RED, GREEN, BLUE, 1.0f)
-				.texture(0.0F + progress, 0.0F + progress).overlay(overlay).light(light)
-				.normal(normal, nx, ny, nz).next();
+				.uv(0.0F + progress, 0.0F + progress).overlayCoords(overlay).uv2(light)
+				.normal(normal, nx, ny, nz).endVertex();
 
 		vertexConsumer.vertex(pose, x2, y2, z2).color(RED, GREEN, BLUE, 1.0f)
-				.texture(0.0F + progress, 0.2F + progress).overlay(overlay).light(light)
-				.normal(normal, nx, ny, nz).next();
+				.uv(0.0F + progress, 0.2F + progress).overlayCoords(overlay).uv2(light)
+				.normal(normal, nx, ny, nz).endVertex();
 
 		vertexConsumer.vertex(pose, x3, y3, z3).color(RED, GREEN, BLUE, 1.0f)
-				.texture(0.2F + progress, 0.2F + progress).overlay(overlay).light(light)
-				.normal(normal, nx, ny, nz).next();
+				.uv(0.2F + progress, 0.2F + progress).overlayCoords(overlay).uv2(light)
+				.normal(normal, nx, ny, nz).endVertex();
 
 		vertexConsumer.vertex(pose, x4, y4, z4).color(RED, GREEN, BLUE, 1.0f)
-				.texture(0.2F + progress, 0.0F + progress).overlay(overlay).light(light)
-				.normal(normal, nx, ny, nz).next();
+				.uv(0.2F + progress, 0.0F + progress).overlayCoords(overlay).uv2(light)
+				.normal(normal, nx, ny, nz).endVertex();
 	}
 }
