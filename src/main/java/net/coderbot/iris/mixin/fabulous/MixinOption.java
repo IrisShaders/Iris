8106ddb3e5bb39c05c673ba11d5740e510455400
coderbot <coderbot16@gmail.com>
2021-09-03 11:57:04 -0700

    Add signature to MixinOption fabulous graphics redirect

--- src/main/java/net/coderbot/iris/mixin/fabulous/MixinOption.java
+++ src/main/java/net/coderbot/iris/mixin/fabulous/MixinOption.java
@@ -14,7 +14,8 @@ import net.fabricmc.api.Environment;
 @Environment(EnvType.CLIENT)
 @Mixin(Option.class)
 public class MixinOption {
-	@Redirect(method = "method_18554", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GlStateManager;supportsGl30()Z"))
+	@Redirect(method = "method_18554(Lnet/minecraft/client/options/GameOptions;Ljava/lang/Integer;)V",
+			at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GlStateManager;supportsGl30()Z"))
 	private static boolean iris$onAttemptedToSelectFabulousGraphics() {
 		// Returning false here will cause Minecraft to cycle between Fancy and Fast, disabling Fabulous graphics
 		if (!Iris.getIrisConfig().areShadersEnabled()) {
