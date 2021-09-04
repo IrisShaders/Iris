83a935557d71a624471479b1551e846386a70e7b
coderbot <coderbot16@gmail.com>
2021-09-04 15:22:33 -0700

    Disable the Max Shadow Distance slider with shader packs that specify a shadow render distance
    Also add a tooltip to explain what it does / why it is disabled.

---
+++ src/main/java/net/coderbot/iris/gui/option/ShadowDistanceOption.java
@@ -0,0 +1,31 @@
+package net.coderbot.iris.gui.option;
+
+import net.coderbot.iris.Iris;
+import net.coderbot.iris.pipeline.WorldRenderingPipeline;
+import net.minecraft.client.gui.widget.AbstractButtonWidget;
+import net.minecraft.client.options.DoubleOption;
+import net.minecraft.client.options.GameOptions;
+import net.minecraft.text.Text;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class ShadowDistanceOption extends DoubleOption {
+	public ShadowDistanceOption(String key, double min, double max, float step, Function<GameOptions, Double> getter, BiConsumer<GameOptions, Double> setter, BiFunction<GameOptions, DoubleOption, Text> displayStringGetter) {
+		super(key, min, max, step, getter, setter, displayStringGetter);
+	}
+
+	@Override
+	public AbstractButtonWidget createButton(GameOptions options, int x, int y, int width) {
+		AbstractButtonWidget widget = new ShadowDistanceSliderWidget(options, x, y, width, 20, this);
+
+		WorldRenderingPipeline pipeline = Iris.getPipelineManager().getPipeline();
+
+		if (pipeline != null && pipeline.getForcedShadowRenderDistanceChunksForDisplay().isPresent()) {
+			widget.active = false;
+		}
+
+		return widget;
+	}
+}
