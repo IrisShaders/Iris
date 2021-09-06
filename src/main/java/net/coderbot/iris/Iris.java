c9ba4a81049f5ef1b2f210c2d3670427185d5dd2
coderbot <coderbot16@gmail.com>
2021-09-05 18:26:19 -0700

    Iris 1.1.2, bump Sodium fork version to 0.2.0+IRIS4

--- src/main/java/net/coderbot/iris/Iris.java
+++ src/main/java/net/coderbot/iris/Iris.java
@@ -39,7 +39,7 @@ public class Iris implements ClientModInitializer {
 	public static final String MODID = "iris";
 	public static final Logger logger = LogManager.getLogger(MODID);
 	// The recommended version of Sodium for use with Iris
-	private static final String SODIUM_VERSION = "0.2.0+IRIS3";
+	private static final String SODIUM_VERSION = "0.2.0+IRIS4";
 
 	public static final Path SHADERPACKS_DIRECTORY = FabricLoader.getInstance().getGameDir().resolve("shaderpacks");
 
