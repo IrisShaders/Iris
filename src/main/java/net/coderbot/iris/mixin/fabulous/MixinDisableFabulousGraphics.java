22eedfc2c5add5553b57feb270d1cdd5c1998c1d
IMS212 <31803019+IMS212@users.noreply.github.com>
2021-09-14 16:36:14 -0700

    Add the fabulous graphics disabling code

--- src/main/java/net/coderbot/iris/mixin/fabulous/MixinDisableFabulousGraphics.java
+++ src/main/java/net/coderbot/iris/mixin/fabulous/MixinDisableFabulousGraphics.java
@@ -2,6 +2,7 @@ package net.coderbot.iris.mixin.fabulous;
 
 import net.coderbot.iris.Iris;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,10 +17,28 @@ import net.minecraft.client.renderer.LevelRenderer;
 @Mixin(LevelRenderer.class)
 public class MixinDisableFabulousGraphics {
 	@Inject(method = "onResourceManagerReload", at = @At("HEAD"))
-	private void iris$disableFabulousGraphics(CallbackInfo ci) {
+	private void iris$disableFabulousGraphicsOnResourceReload(CallbackInfo ci) {
+		iris$disableFabulousGraphics();
+	}
+
+	// This method is called whenever the user tries to change the graphics mode.
+	// We can still revert / intercept the change at the head of the method.
+	@Inject(method = "allChanged", at = @At("HEAD"))
+	private void iris$disableFabulousGraphicsOnLevelRendererReload(CallbackInfo ci) {
+		iris$disableFabulousGraphics();
+	}
+
+	@Unique
+	private void iris$disableFabulousGraphics() {
 		Options options = Minecraft.getInstance().options;
 
-		if (options.graphicsMode == GraphicsStatus.FABULOUS && Iris.getIrisConfig().areShadersEnabled()) {
+		if (Iris.getIrisConfig().areShadersEnabled()) {
+			// Nothing to do here, shaders are disabled.
+			return;
+		}
+
+		if (options.graphicsMode == GraphicsStatus.FABULOUS) {
+			// Disable fabulous graphics when shaders are enabled.
 			options.graphicsMode = GraphicsStatus.FANCY;
 		}
 	}
