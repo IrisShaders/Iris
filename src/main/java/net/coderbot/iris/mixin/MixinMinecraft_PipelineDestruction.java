1a6c0a6db5988f1a110ffe32a2c33e4dffda2970
coderbot <coderbot16@gmail.com>
2021-09-27 21:52:49 -0700

    Missed rename: MinecraftClient -> Minecraft

--- src/main/java/net/coderbot/iris/mixin/MixinMinecraft_PipelineDestruction.java
+++ src/main/java/net/coderbot/iris/mixin/MixinMinecraft_PipelineDestruction.java
@@ -10,9 +10,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+/**
+ * Ensures that all pipelines are destroyed when changing to a different dimension.
+ */
 @Mixin(Minecraft.class)
 @Environment(EnvType.CLIENT)
-public class MixinMinecraftClient {
+public class MixinMinecraft_PipelineDestruction {
 	@Inject(method = "setLevel", at = @At("RETURN"))
 	private void iris$dispose(ClientLevel world, CallbackInfo ci) {
 		Iris.getPipelineManager().destroyPipeline();
