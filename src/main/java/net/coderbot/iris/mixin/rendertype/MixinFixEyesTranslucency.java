a6b5538942ad46b941263eb6d0dd036fec2f6eab
coderbot <coderbot16@gmail.com>
2021-09-27 21:50:23 -0700

    Fix MixinFixEyesTranslucency not compiling

--- src/main/java/net/coderbot/iris/mixin/rendertype/MixinFixEyesTranslucency.java
+++ src/main/java/net/coderbot/iris/mixin/rendertype/MixinFixEyesTranslucency.java
@@ -1,13 +1,8 @@
 package net.coderbot.iris.mixin.rendertype;
 
-import net.coderbot.iris.mixin.fantastic.RenderStateShardAccessor;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 
 @Environment(EnvType.CLIENT)
