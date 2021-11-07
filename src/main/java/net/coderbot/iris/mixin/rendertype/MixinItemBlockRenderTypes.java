3d68cb8ca236fb226de2147ca1f8809de32d0134
maximumpower55 <62370320+maximumpower55@users.noreply.github.com>
2021-11-06 17:12:16 -0700

    fix render types this time i hope

--- src/main/java/net/coderbot/iris/mixin/rendertype/MixinItemBlockRenderTypes.java
+++ src/main/java/net/coderbot/iris/mixin/rendertype/MixinItemBlockRenderTypes.java
@@ -8,13 +8,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.coderbot.batchedentityrendering.impl.WrappableRenderType;
 import net.coderbot.iris.layer.GbufferProgram;
 import net.coderbot.iris.layer.IrisRenderTypeWrapper;
 import net.coderbot.iris.layer.UseProgramRenderStateShard;
 import net.coderbot.iris.pipeline.HandRenderer;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 
 @Mixin(ItemBlockRenderTypes.class)
 public abstract class MixinItemBlockRenderTypes {
@@ -32,6 +33,20 @@ public abstract class MixinItemBlockRenderTypes {
 
 		return wrap("iris:" + name, wrapped, program);
 	}
+
+	@Inject(method = "getRenderType(Lnet/minecraft/world/item/ItemStack;Z)Lnet/minecraft/client/renderer/RenderType;", at = @At("RETURN"), cancellable = true)
+	private static void getRenderType(ItemStack itemStack, boolean bl, CallbackInfoReturnable<RenderType> cir) {
+		RenderType base = cir.getReturnValue();
+
+		if(HandRenderer.isActive()) {
+			//TODO: Is there a better way to do this?
+			if(base == Sheets.translucentItemSheet()) {
+				cir.setReturnValue(RenderType.entityTranslucentCull(TextureAtlas.LOCATION_BLOCKS));
+			} else if(base == Sheets.translucentCullBlockSheet()) {
+				cir.setReturnValue(RenderType.itemEntityTranslucentCull(TextureAtlas.LOCATION_BLOCKS));
+			}
+		}
+	 }
 
 	@Inject(method = "getRenderType(Lnet/minecraft/world/level/block/state/BlockState;Z)Lnet/minecraft/client/renderer/RenderType;", at = @At("RETURN"), cancellable = true)
 	private static void getRenderType(BlockState blockState, boolean bl, CallbackInfoReturnable<RenderType> cir) {
