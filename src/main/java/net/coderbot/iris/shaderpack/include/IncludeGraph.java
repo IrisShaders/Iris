e47c188628470da5992eac666f56dcd5fbef236e
coderbot16 <coderbot16@gmail.com>
2021-11-21 13:29:00 -0800

    Add a class doc comment to IncludeGraph

--- src/main/java/net/coderbot/iris/shaderpack/include/IncludeGraph.java
+++ src/main/java/net/coderbot/iris/shaderpack/include/IncludeGraph.java
@@ -15,6 +15,41 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * A graph data structure that holds the loaded source of all shader programs
+ * and the files included by each source file. Each node / vertex in the graph
+ * corresponds to a single file in the shader pack, and each edge / connection
+ * corresponds to an {@code #include} directive on a given line.
+ *
+ * <p>Using a proper graph representation allows us to apply existing and
+ * efficient algorithms with well-known properties to various tasks and
+ * transformations necessary during shader pack loading. We receive a number of
+ * immediate benefits from using a graph-based representation:</p>
+ *
+ * <ul>
+ *     <li>Each file is read exactly one time, and it is only necessary to
+ *         parse #include directives from a file once. This ensures efficient
+ *         IO.
+ *         </li>
+ *     <li>Deferring the processing of inclusions allows transformers that only
+ *         need to replace single lines at a time to operate more efficiently,
+ *         avoiding processing lines duplicated across many files more than
+ *         necessary.
+ *         </li>
+ *     <li>As a result, our shader configuration system is able to process and
+ *         apply options much more efficiently than a naive one operating on
+ *         included files only, allowing many operations to scale much more
+ *         nicely, especially in the common case of shader pack authors having
+ *         a single large settings file defining every config option that is
+ *         then included in every shader program in the pack.
+ *         </li>
+ *     <li>Deferred processing of inclusions also allows the shader pack loader
+ *         to reason about cyclic inclusions, allowing us to remove the
+ *         arbitrary file include depth limit, and avoid stack overflows due to
+ *         infinite recursion that a naive implementation might be subject to.
+ *         </li>
+ * </ul>
+ */
 // TODO: Write tests for this code
 public class IncludeGraph {
 	private final ImmutableMap<AbsolutePackPath, FileNode> nodes;
