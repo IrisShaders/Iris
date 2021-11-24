3df071c326e95e66ef018bd4d0489c0281e8752b
IMS212 <ims@ims21.tech>
2021-11-23 17:48:37 -0800

    Don't overwrite FrustumAdapter code

--- src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/shadow_map/frustum/MixinBoxCullingFrustum.java
+++ src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/shadow_map/frustum/MixinBoxCullingFrustum.java
@@ -1,6 +1,7 @@
 package net.coderbot.iris.compat.sodium.mixin.shadow_map.frustum;
 
 import me.jellysquid.mods.sodium.client.util.frustum.Frustum;
+import me.jellysquid.mods.sodium.client.util.frustum.FrustumAdapter;
 import net.coderbot.iris.shadows.frustum.BoxCuller;
 import net.coderbot.iris.shadows.frustum.fallback.BoxCullingFrustum;
 import org.spongepowered.asm.mixin.Final;
@@ -8,7 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(BoxCullingFrustum.class)
-public class MixinBoxCullingFrustum implements Frustum {
+public class MixinBoxCullingFrustum implements Frustum, FrustumAdapter {
 	@Shadow
 	@Final
 	private BoxCuller boxCuller;
@@ -18,5 +19,10 @@ public class MixinBoxCullingFrustum implements Frustum {
 	public Visibility testBox(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
 		// TODO: Frustum.INSIDE
 		return this.boxCuller.isCulled(minX, minY, minZ, maxX, maxY, maxZ) ? Visibility.OUTSIDE : Visibility.INTERSECT;
+	}
+
+	@Override
+	public Frustum sodium$createFrustum() {
+		return this;
 	}
 }
