303e9fd84985c7f402b3bb4b7e78e7b2bd7458b4
coderbot <coderbot16@gmail.com>
2021-11-28 22:54:03 -0800

    Awful hack to get JCPP to handle #extension and #version more robustly

--- src/main/java/net/coderbot/iris/shaderpack/preprocessor/JcppProcessor.java
+++ src/main/java/net/coderbot/iris/shaderpack/preprocessor/JcppProcessor.java
@@ -1,42 +1,36 @@
 package net.coderbot.iris.shaderpack.preprocessor;
 
-import net.coderbot.iris.Iris;
-import org.anarres.cpp.DefaultPreprocessorListener;
 import org.anarres.cpp.Feature;
 import org.anarres.cpp.Preprocessor;
 import org.anarres.cpp.StringLexerSource;
 import org.anarres.cpp.Token;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class JcppProcessor {
 	// Derived from GlShader from Canvas, licenced under LGPL
 	public static String glslPreprocessSource(String source) {
-		// NB: This doesn't work when ran in the above methods, only directly when creating the pass for some reason.
-		// The C preprocessor won't understand the #version token, so we must remove it and readd it later.
-
-		source = source.substring(source.indexOf("#version"));
-		int versionStringEnd = source.indexOf('\n');
-
-		String versionLine = source.substring(0, versionStringEnd);
-		source = source.substring(versionStringEnd + 1);
-
-		List<String> extensionLines = new ArrayList<>();
-
-		while (source.contains("#extension")) {
-			int extensionLineStart = source.indexOf("#extension");
-			int extensionLineEnd = source.indexOf("\n", extensionLineStart);
-
-			String extensionLine = source.substring(extensionLineStart, extensionLineEnd);
-			source = source.replaceFirst(extensionLine, "");
-
-			extensionLines.add(extensionLine);
+		if (source.contains(GlslCollectingListener.VERSION_MARKER)
+				|| source.contains(GlslCollectingListener.EXTENSION_MARKER)) {
+			throw new RuntimeException("Some shader author is trying to exploit internal Iris implementation details, stop!");
 		}
+
+		// Note: This is an absolutely awful hack. But JCPP's lack of extensibility leaves me with no choice...
+		//       We should write our own preprocessor at some point to avoid this.
+		//
+		// Why are we doing this awful hack instead of just using the preprocessor like a normal person? Because it lets
+		// us only hoist #extension directives if they're actually used. This is needed for shader packs written on
+		// lenient drivers that allow #extension directives to be placed anywhere to work on strict drivers like Mesa
+		// that require #extension directives to occur at the top.
+		//
+		// TODO: This allows #version to not appear as the first non-comment non-whitespace thing in the file.
+		//       That's not the behavior we want. If you're reading this, don't rely on this behavior.
+		source = source.replace("#version", GlslCollectingListener.VERSION_MARKER);
+		source = source.replace("#extension", GlslCollectingListener.EXTENSION_MARKER);
+
+		GlslCollectingListener listener = new GlslCollectingListener();
 
 		@SuppressWarnings("resource")
 		final Preprocessor pp = new Preprocessor();
-		pp.setListener(new DefaultPreprocessorListener());
+		pp.setListener(listener);
 		pp.addInput(new StringLexerSource(source, true));
 		pp.addFeature(Feature.KEEPCOMMENTS);
 
@@ -55,15 +49,7 @@ public class JcppProcessor {
 
 		builder.append("\n");
 
-		// restore extensions
-		for (String line : extensionLines) {
-			builder.insert(0, line + "\n");
-		}
-
-		// restore GLSL version
-		builder.insert(0, versionLine + "\n");
-
-		source = builder.toString();
+		source = listener.collectLines() + builder;
 
 		return source;
 	}
