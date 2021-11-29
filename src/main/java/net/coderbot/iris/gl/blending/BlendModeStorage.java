9ed5713cd00f097a6d540d8feead72c637a0518a
coderbot <coderbot16@gmail.com>
2021-11-28 20:42:07 -0800

    BlendMode: Switch from public fields to private final fields with getters

--- src/main/java/net/coderbot/iris/gl/blending/BlendModeStorage.java
+++ src/main/java/net/coderbot/iris/gl/blending/BlendModeStorage.java
@@ -15,12 +15,12 @@ public class BlendModeStorage {
 		blendLocked = false;
 		GlStateManager.BlendState blendState = GlStateManagerAccessor.getBLEND();
 		originalBlend = new BlendMode(blendState.srcRgb, blendState.dstRgb, blendState.srcAlpha, blendState.dstAlpha);
-		GlStateManager._blendFuncSeparate(override.srcRgb, override.dstRgb, override.srcAlpha, override.dstAlpha);
+		GlStateManager._blendFuncSeparate(override.getSrcRgb(), override.getDstRgb(), override.getSrcAlpha(), override.getDstAlpha());
 		blendLocked = true;
 	}
 
 	public static void restoreBlend() {
 		blendLocked = false;
-		GlStateManager._blendFuncSeparate(originalBlend.srcRgb, originalBlend.dstRgb, originalBlend.srcAlpha, originalBlend.dstAlpha);
+		GlStateManager._blendFuncSeparate(originalBlend.getSrcRgb(), originalBlend.getDstRgb(), originalBlend.getSrcAlpha(), originalBlend.getDstAlpha());
 	}
 }
