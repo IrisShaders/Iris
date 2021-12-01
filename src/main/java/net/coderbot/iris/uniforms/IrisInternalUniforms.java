06f14ec8e3871f8d54eb685c548218ca45e73099
coderbot <coderbot16@gmail.com>
2021-11-30 20:52:15 -0800

    Refactor: FogUniforms117 -> IrisInternalUniforms

--- src/main/java/net/coderbot/iris/uniforms/IrisInternalUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/IrisInternalUniforms.java
@@ -7,18 +7,17 @@ import net.coderbot.iris.gl.uniform.UniformHolder;
 
 import static net.coderbot.iris.gl.uniform.UniformUpdateFrequency.PER_FRAME;
 
-public class FogUniforms117 {
-	private FogUniforms117() {
+/**
+ * Internal Iris uniforms that are not directly accessible by shaders.
+ */
+public class IrisInternalUniforms {
+	private IrisInternalUniforms() {
 		// no construction
 	}
 
 	public static void addFogUniforms(UniformHolder uniforms) {
 		uniforms
 				// TODO: Update frequency of continuous?
-				.uniform3f(PER_FRAME, "fogColor", () -> {
-					float[] fogColor = RenderSystem.getShaderFogColor();
-					return new Vector3f(fogColor[0], fogColor[1], fogColor[2]);
-				})
 				.uniform4f(PER_FRAME, "iris_FogColor", () -> {
 					float[] fogColor = RenderSystem.getShaderFogColor();
 					return new Vector4f(fogColor[0], fogColor[1], fogColor[2], fogColor[3]);
