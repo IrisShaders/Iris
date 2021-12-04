17972df3a0f0302c31c71427d94b852b02e68c45
coderbot <coderbot16@gmail.com>
2021-12-04 15:26:26 -0800

    fix: Ensure that cameraPosition does not lose precision at far distances
    - https://github.com/IrisShaders/Iris/issues/423
    - https://github.com/IrisShaders/Iris/issues/334

--- src/main/java/net/coderbot/iris/uniforms/CameraUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/CameraUniforms.java
@@ -37,9 +37,11 @@ public class CameraUniforms {
 	}
 
 	private static class CameraPositionTracker {
-		private Vector3d previousCameraPosition = new Vector3d(0.0, 0.0, 0.0);
-		private Vector3d currentCameraPosition = new Vector3d(0.0, 0.0, 0.0);
-		private Vector3d shift = new Vector3d();
+		private static final double RANGE = 1024.0;
+
+		private Vector3d previousCameraPosition = new Vector3d();
+		private Vector3d currentCameraPosition = new Vector3d();
+		private final Vector3d shift = new Vector3d();
 
 		private CameraPositionTracker(FrameUpdateNotifier notifier) {
 			notifier.addListener(this::update);
@@ -48,6 +50,53 @@ public class CameraUniforms {
 		private void update() {
 			previousCameraPosition = currentCameraPosition;
 			currentCameraPosition = getUnshiftedCameraPosition().add(shift);
+
+			updateShift();
+		}
+
+		/**
+		 * Updates our shift values to try to keep |x| < 1024 and |z| < 1024, to maintain precision with cameraPosition.
+		 * Since our actual range is 2048x2048, this means that we won't excessively move back and forth when moving
+		 * around a chunk border.
+		 */
+		private void updateShift() {
+			double dX;
+			double dZ;
+
+			if (currentCameraPosition.x > RANGE) {
+				dX = -currentCameraPosition.x;
+			} else if (currentCameraPosition.x < -RANGE) {
+				dX = currentCameraPosition.x;
+			} else {
+				dX = 0.0;
+			}
+
+			if (currentCameraPosition.z > RANGE) {
+				dZ = -currentCameraPosition.z;
+			} else if (currentCameraPosition.z < -RANGE) {
+				dZ = currentCameraPosition.z;
+			} else {
+				dZ = 0.0;
+			}
+
+			if (dX != 0.0 || dZ != 0.0) {
+				applyShift(dX, dZ);
+			}
+		}
+
+		/**
+		 * Shifts all current and future positions by the given amount. This is done in such a way that the difference
+		 * between cameraPosition and previousCameraPosition remains the same, since they are completely arbitrary
+		 * to the shader for the most part.
+		 */
+		private void applyShift(double dX, double dZ) {
+			shift.x += dX;
+			currentCameraPosition.x += dX;
+			previousCameraPosition.x += dX;
+
+			shift.z += dZ;
+			currentCameraPosition.z += dZ;
+			previousCameraPosition.z += dZ;
 		}
 
 		public Vector3d getCurrentCameraPosition() {
