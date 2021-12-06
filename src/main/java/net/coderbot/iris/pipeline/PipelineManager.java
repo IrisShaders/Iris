120ea029be9f4f5f25cc3ec4746a9e021faffa91
coderbot <coderbot16@gmail.com>
2021-12-06 00:00:42 -0800

    Port the 1.17+ PipelineManager changes to 1.16

--- src/main/java/net/coderbot/iris/pipeline/PipelineManager.java
+++ src/main/java/net/coderbot/iris/pipeline/PipelineManager.java
@@ -8,50 +8,37 @@ import net.coderbot.iris.uniforms.SystemTimeUniforms;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.opengl.GL20C;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class PipelineManager {
 	private static PipelineManager instance;
 	private final Function<DimensionId, WorldRenderingPipeline> pipelineFactory;
+	private final Map<DimensionId, WorldRenderingPipeline> pipelinesPerDimension = new HashMap<>();
 	private WorldRenderingPipeline pipeline;
 	private boolean sodiumShaderReloadNeeded;
-	private DimensionId lastDimension;
 
 	public PipelineManager(Function<DimensionId, WorldRenderingPipeline> pipelineFactory) {
 		this.pipelineFactory = pipelineFactory;
 	}
 
 	public WorldRenderingPipeline preparePipeline(DimensionId currentDimension) {
-		if (currentDimension != lastDimension) {
-			// TODO: Don't say anything about compiling shaders if shaders are disabled.
-			if (lastDimension == null) {
-				Iris.logger.info("Compiling shaderpack on initial world load (for dimension: " + currentDimension + ")");
-			} else {
-				Iris.logger.info("Recompiling shaderpack on dimension change (" + lastDimension + " -> " + currentDimension + ")");
-			}
-
-			lastDimension = currentDimension;
-			destroyPipeline();
-		}
-
-		if (pipeline == null) {
-			// Ensure that the timers are reset
+		if (!pipelinesPerDimension.containsKey(currentDimension)) {
 			SystemTimeUniforms.COUNTER.reset();
 			SystemTimeUniforms.TIMER.reset();
 
-			pipeline = pipelineFactory.apply(lastDimension);
+			Iris.logger.info("Creating pipeline for dimension {}", currentDimension);
+			pipeline = pipelineFactory.apply(currentDimension);
+			pipelinesPerDimension.put(currentDimension, pipeline);
 			sodiumShaderReloadNeeded = true;
 
-			// If Sodium is loaded, we need to reload the world renderer to properly recreate the ChunkRenderBackend
-			// Otherwise, the terrain shaders won't be changed properly.
-			// We also need to re-render all of the chunks if there is a change in the directional shading setting,
-			// ID mapping, or separateAo setting.
-			//
-			// TODO: Don't trigger a reload if this is the first time the world is being rendered
 			if (BlockRenderingSettings.INSTANCE.isReloadRequired()) {
 				Minecraft.getInstance().levelRenderer.allChanged();
 				BlockRenderingSettings.INSTANCE.clearReloadRequired();
 			}
+		} else {
+			pipeline = pipelinesPerDimension.get(currentDimension);
 		}
 
 		return pipeline;
@@ -86,6 +73,17 @@ public class PipelineManager {
 	}
 
 	public void destroyPipeline() {
+		pipelinesPerDimension.forEach((dimensionId, pipeline) -> {
+			Iris.logger.info("Destroying pipeline {}", dimensionId);
+			resetTextureState();
+			pipeline.destroy();
+		});
+
+		pipelinesPerDimension.clear();
+		pipeline = null;
+	}
+
+	private void resetTextureState() {
 		// Unbind all textures
 		//
 		// This is necessary because we don't want destroyed render target textures to remain bound to certain texture
@@ -94,22 +92,13 @@ public class PipelineManager {
 		//
 		// Without this code, there will be weird issues when reloading certain shaderpacks.
 		for (int i = 0; i < 16; i++) {
-			GlStateManager._activeTexture(GL20C.GL_TEXTURE0 + i);
+			GlStateManager.glActiveTexture(GL20C.GL_TEXTURE0 + i);
 			GlStateManager._bindTexture(0);
 		}
 
 		// Set the active texture unit to unit 0
 		//
 		// This seems to be what most code expects. It's a sane default in any case.
-		GlStateManager._activeTexture(GL20C.GL_TEXTURE0);
-
-		// Destroy the old world rendering pipeline
-		//
-		// This destroys all loaded shader programs and all of the render targets.
-		if (pipeline != null) {
-			pipeline.destroy();
-		}
-
-		pipeline = null;
+		GlStateManager.glActiveTexture(GL20C.GL_TEXTURE0);
 	}
 }
