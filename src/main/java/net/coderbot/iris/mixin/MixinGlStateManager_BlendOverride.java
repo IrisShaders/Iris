09f604f4f44c809910aa049108e895e22ab1be87
IMS212 <ims@ims21.tech>
2022-01-03 13:33:26 -0800

    Move blendFuncListener

--- src/main/java/net/coderbot/iris/mixin/MixinGlStateManager_BlendOverride.java
+++ src/main/java/net/coderbot/iris/mixin/MixinGlStateManager_BlendOverride.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GlStateManager.class)
 public class MixinGlStateManager_BlendOverride {
-	private static Runnable blendFuncListener;
 
 	@Inject(method = "_disableBlend", at = @At("HEAD"), cancellable = true)
 	private static void iris$blendDisableLock(CallbackInfo ci) {
@@ -35,9 +34,7 @@ public class MixinGlStateManager_BlendOverride {
 			ci.cancel();
 		}
 
-		if (blendFuncListener != null) {
-			blendFuncListener.run();
-		}
+
 	}
 
 	@Inject(method = "_blendFuncSeparate", at = @At("HEAD"), cancellable = true)
@@ -46,13 +43,5 @@ public class MixinGlStateManager_BlendOverride {
 			BlendModeStorage.deferBlendFunc(srcRgb, dstRgb, srcAlpha, dstAlpha);
 			ci.cancel();
 		}
-
-		if (blendFuncListener != null) {
-			blendFuncListener.run();
-		}
-	}
-
-	static {
-		StateUpdateNotifiers.blendFuncNotifier = listener -> blendFuncListener = listener;
 	}
 }
