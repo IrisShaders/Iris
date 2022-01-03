a75059f3924f9ff14bdea3114c6acaaffb033621
IMS212 <ims@ims21.tech>
2022-01-03 08:05:33 -0800

    Dynamically change fabulous graphics mode during save

--- src/main/java/net/coderbot/iris/mixin/fabulous/MixinDisableFabulousGraphics.java
+++ src/main/java/net/coderbot/iris/mixin/fabulous/MixinDisableFabulousGraphics.java
@@ -1,12 +1,11 @@
 package net.coderbot.iris.mixin.fabulous;
 
 import net.coderbot.iris.Iris;
+import net.coderbot.iris.fantastic.FabulousInterface;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -16,24 +15,44 @@ import net.minecraft.client.Options;
 import net.minecraft.client.renderer.LevelRenderer;
 
 @Environment(EnvType.CLIENT)
-@Mixin(Minecraft.class)
-public class MixinDisableFabulousGraphics {
-	/**
-	 * @author IMS
-	 * @reason return Fancy over Fabulous if shaders are enabled
-	 */
-	@Overwrite
-	public static boolean useShaderTransparency() {
-		return Minecraft.getInstance().options.graphicsMode.getId() >= GraphicsStatus.FABULOUS.getId() && !Iris.isPackActive();
+@Mixin(LevelRenderer.class)
+public class MixinDisableFabulousGraphics implements FabulousInterface {
+	private boolean wasFabulous;
+
+	@Inject(method = "onResourceManagerReload", at = @At("HEAD"))
+	private void iris$disableFabulousGraphicsOnResourceReload(CallbackInfo ci) {
+		iris$disableFabulousGraphics();
 	}
 
-	// This field is used for the F3 screen.
-	@Redirect(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/GraphicsStatus;toString()Ljava/lang/String;"))
-	public String changeFpsString(GraphicsStatus instance) {
-		if (instance == GraphicsStatus.FABULOUS && Iris.isPackActive()) {
-			return "fancy";
-		} else {
-			return instance.toString();
+	// This method is called whenever the user tries to change the graphics mode.
+	// We can still revert / intercept the change at the head of the method.
+	@Inject(method = "allChanged", at = @At("HEAD"))
+	private void iris$disableFabulousGraphicsOnLevelRendererReload(CallbackInfo ci) {
+		iris$disableFabulousGraphics();
+	}
+
+	@Unique
+	private void iris$disableFabulousGraphics() {
+		Options options = Minecraft.getInstance().options;
+
+		if (!Iris.getIrisConfig().areShadersEnabled()) {
+			// Nothing to do here, shaders are disabled.
+			if (this.wasFabulous) {
+				options.graphicsMode = GraphicsStatus.FABULOUS;
+				this.wasFabulous = false;
+			}
+			return;
 		}
+
+		if (options.graphicsMode == GraphicsStatus.FABULOUS) {
+			// Disable fabulous graphics when shaders are enabled.
+			options.graphicsMode = GraphicsStatus.FANCY;
+			this.wasFabulous = true;
+		}
+	}
+
+	@Override
+	public boolean wasFabulous() {
+		return wasFabulous;
 	}
 }
---
+++ src/main/java/net/coderbot/iris/mixin/fabulous/MixinEnableFabulousGraphics.java
@@ -0,0 +1,34 @@
+package net.coderbot.iris.mixin.fabulous;
+
+import net.coderbot.iris.fantastic.FabulousInterface;
+import net.minecraft.client.GraphicsStatus;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Options.class)
+public class MixinEnableFabulousGraphics {
+	@Shadow
+	protected Minecraft minecraft;
+
+	@Shadow
+	public GraphicsStatus graphicsMode;
+
+	@Inject(method = "save", at = @At("HEAD"))
+	private void onSave(CallbackInfo ci) {
+		if (((FabulousInterface) this.minecraft.levelRenderer).wasFabulous()) {
+			this.graphicsMode = GraphicsStatus.FABULOUS;
+		}
+	}
+
+	@Inject(method = "save", at = @At("TAIL"))
+	private void afterSave(CallbackInfo ci) {
+		if (((FabulousInterface) this.minecraft.levelRenderer).wasFabulous()) {
+			this.graphicsMode = GraphicsStatus.FANCY;
+		}
+	}
+}
