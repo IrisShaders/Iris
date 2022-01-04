b3e1643dff09c79c2b5828e46dafb78d976e915f
maximumpower55 <62370320+maximumpower55@users.noreply.github.com>
2022-01-03 20:48:48 -0800

    Add a workaround for when OptiFine has set the maxFps to zero in options.txt

---
+++ src/main/java/net/coderbot/iris/mixin/MixinMaxFpsCrashFix.java
@@ -0,0 +1,32 @@
+package net.coderbot.iris.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+import net.minecraft.client.Options;
+
+/**
+ * A workaround for when OptiFine has set the maxFps to zero in options.txt
+ * 
+ * Fun.
+ */
+@Mixin(Options.class)
+public abstract class MixinMaxFpsCrashFix {
+	@Redirect(
+		method = "load",
+		at = @At(value = "INVOKE", target = "Ljava/lang/Integer;parseInt(Ljava/lang/String;)I"),
+		slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=maxFps"), to = @At(value = "CONSTANT", args = "stringValue=difficulty"))
+	)
+	private int iris$resetFramerateLimit(String string) {
+		int original = Integer.parseInt(string);
+
+		if (original == 0) {
+			// Return the default value of framerateLimit
+			return 120;
+		}
+
+		return original;
+	}
+}
