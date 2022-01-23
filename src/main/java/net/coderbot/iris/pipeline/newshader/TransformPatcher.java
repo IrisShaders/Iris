051710cd8062ded78bbb391faab931f6a8dcec6a
douira <douira100@gmail.com>
2022-01-23 02:48:14 +0100

    added access to job parameters

--- src/main/java/net/coderbot/iris/pipeline/newshader/TransformPatcher.java
+++ src/main/java/net/coderbot/iris/pipeline/newshader/TransformPatcher.java
@@ -42,21 +42,56 @@ import net.coderbot.iris.shaderpack.transform.Transformations;
 public class TransformPatcher implements Patcher {
   private static final Logger LOGGER = LogManager.getLogger(TransformPatcher.class);
 
-  private Table<Patch, ShaderType, TransformationManager> managers = HashBasedTable.create(
-      Patch.values().length,
-      ShaderType.values().length);
-
   private static enum Patch {
     VANILLA, SODIUM, COMPOSITE
   }
+
+  private static class VanillaParameters {
+    public final AlphaTest alpha;
+    public final boolean hasChunkOffset;
+    public final ShaderAttributeInputs inputs;
+
+    public VanillaParameters(AlphaTest alpha, boolean hasChunkOffset, ShaderAttributeInputs inputs) {
+      this.alpha = alpha;
+      this.hasChunkOffset = hasChunkOffset;
+      this.inputs = inputs;
+    }
+  }
+
+  private static class SodiumParameters {
+    public final AlphaTest alpha;
+    public final ShaderAttributeInputs inputs;
+    public final float positionScale;
+    public final float positionOffset;
+    public final float textureScale;
+
+    public SodiumParameters(AlphaTest alpha, ShaderAttributeInputs inputs, float positionScale,
+        float positionOffset, float textureScale) {
+      this.alpha = alpha;
+      this.inputs = inputs;
+      this.positionScale = positionScale;
+      this.positionOffset = positionOffset;
+      this.textureScale = textureScale;
+    }
+  }
+
+  /**
+   * A transformation manager is kept for each patch type and shader type for
+   * better performance since it allows the phase collector to compact the whole
+   * run better. The object parameter can be filled with one of the parameter
+   * classes and the transformation phases have to do their own casts.
+   */
+  private Table<Patch, ShaderType, TransformationManager<Object>> managers = HashBasedTable.create(
+      Patch.values().length,
+      ShaderType.values().length);
 
   public TransformPatcher() {
     // PREV TODO: Only do the NewLines patches if the source code isn't from
     // gbuffers_lines
 
     // setup the transformations and even loose phases if necessary
-    Transformation detectReserved = new Transformation(
-        new SearchTerminals(SearchTerminals.IDENTIFIER,
+    Transformation<Object> detectReserved = new Transformation<>(
+        new SearchTerminals<>(SearchTerminals.IDENTIFIER,
             ImmutableSet.of(
                 ThrowTarget.fromMessage(
                     "moj_import", "Iris shader programs may not use moj_import directives."),
@@ -64,7 +99,7 @@ public class TransformPatcher implements Patcher {
                     "iris_",
                     "Detected a potential reference to unstable and internal Iris shader interfaces (iris_). This isn't currently supported."))));
 
-    Transformation fixVersion = new Transformation(new RunPhase() {
+    Transformation<Object> fixVersion = new Transformation<>(new RunPhase<Object>() {
       /**
        * This largely replicates the behavior of
        * {@link net.coderbot.iris.pipeline.newshader.TriforcePatcher#fixVersion(Transformations)}
@@ -105,7 +140,7 @@ public class TransformPatcher implements Patcher {
      * passes. A shader that relies on this behavior is SEUS v11 - it reads
      * gl_Fog.color and breaks if it is not properly defined.
      */
-    TransformationPhase sharedFogSetup = new RunPhase() {
+    TransformationPhase<Object> sharedFogSetup = new RunPhase<Object>() {
       @Override
       protected void run(TranslationUnitContext ctx) {
         injectExternalDeclaration(
@@ -127,11 +162,14 @@ public class TransformPatcher implements Patcher {
 
     // PREV TODO: What if the shader does gl_PerVertex.gl_FogFragCoord ?
     // TODO: update to use new glsl-transformer features to make this compact
-    ReplaceTerminals wrapFogFragCoord = new ReplaceTerminals(ReplaceTerminals.IDENTIFIER);
-    wrapFogFragCoord.addReplacementTerminal("gl_FogFragCoord", "iris_FogFragCoord");
+    SearchTerminals<Object> wrapFogFragCoord = new SearchTerminals<Object>() {
+      {
+        addReplacementTerminal("gl_FogFragCoord", "iris_FogFragCoord");
+      }
+    };
 
     // PREV TODO: This doesn't handle geometry shaders... How do we do that?
-    TransformationPhase injectOutFogFragCoord = new RunPhase() {
+    TransformationPhase<Object> injectOutFogFragCoord = new RunPhase<Object>() {
       @Override
       protected void run(TranslationUnitContext ctx) {
         injectExternalDeclaration(
@@ -139,7 +177,7 @@ public class TransformPatcher implements Patcher {
       };
     };
 
-    TransformationPhase injectInFogFragCoord = new RunPhase() {
+    TransformationPhase<Object> injectInFogFragCoord = new RunPhase<Object>() {
       @Override
       protected void run(TranslationUnitContext ctx) {
         injectExternalDeclaration(
@@ -153,26 +191,31 @@ public class TransformPatcher implements Patcher {
      * even though they write to it.
      */
     // TODO: use glsl-transformer core wrapping transformation for this
-    TransformationPhase frontColorInjection = new RunPhase() {
+    TransformationPhase<Object> frontColorInjection = new RunPhase<Object>() {
       @Override
       protected void run(TranslationUnitContext ctx) {
         injectExternalDeclaration(
             "vec4 iris_FrontColor;", InjectionPoint.BEFORE_DECLARATIONS);
       };
     };
-    ReplaceTerminals wrapFrontColor = new ReplaceTerminals(ReplaceTerminals.IDENTIFIER);
-    wrapFogFragCoord.addReplacementTerminal("gl_FrontColor", "iris_FrontColor");
+
+    // TODO use shorthand method
+    SearchTerminals<Object> wrapFrontColor = new SearchTerminals<Object>() {
+      {
+        addReplacementTerminal("gl_FrontColor", "iris_FrontColor");
+      }
+    };
 
     // compose the transformations and phases into the managers
     for (Patch patch : Patch.values()) {
       for (ShaderType type : ShaderType.values()) {
-        TransformationManager manager = new TransformationManager();
+        TransformationManager<Object> manager = new TransformationManager<>();
         managers.put(patch, type, manager);
 
         manager.registerTransformation(detectReserved);
         manager.registerTransformation(fixVersion);
 
-        Transformation commonInjections = new Transformation();
+        Transformation<Object> commonInjections = new Transformation<>();
         manager.registerTransformation(commonInjections);
 
         //TODO: use addConcurrentPhase to make this more compact
@@ -197,18 +240,20 @@ public class TransformPatcher implements Patcher {
   public String patchVanilla(
       String source, ShaderType type, AlphaTest alpha, boolean hasChunkOffset,
       ShaderAttributeInputs inputs) {
-    return managers.get(Patch.VANILLA, type).transform(source);
+    return managers.get(Patch.VANILLA, type)
+        .transform(source, new VanillaParameters(alpha, hasChunkOffset, inputs));
   }
 
   @Override
   public String patchSodium(
       String source, ShaderType type, AlphaTest alpha, ShaderAttributeInputs inputs,
       float positionScale, float positionOffset, float textureScale) {
-    return null;
+    return managers.get(Patch.VANILLA, type)
+        .transform(source, new SodiumParameters(alpha, inputs, positionScale, positionOffset, textureScale));
   }
 
   @Override
   public String patchComposite(String source, ShaderType type) {
-    return null;
+    return managers.get(Patch.VANILLA, type).transform(source, null);
   }
 }
