c21a923681764c85a5c6153f6f712f37406eca88
IMS212 <ims@ims21.tech>
2022-02-03 15:27:14 -0800

    Fix comment

--- src/main/java/net/coderbot/iris/mixin/vertices/MixinVertexBuffer.java
+++ src/main/java/net/coderbot/iris/mixin/vertices/MixinVertexBuffer.java
@@ -21,17 +21,13 @@ public class MixinVertexBuffer {
 
 	@Inject(method = "<init>", at = @At("RETURN"))
 	private void iris$onInit(VertexFormat format, CallbackInfo ci) {
+		// We have to fix the vertex format here, or else the vertex count will be calculated wrongly and too many
+		// vertices will be drawn.
+		//
+		// Needless to say, that is not good if you don't like access violation crashes!
 		if (format == DefaultVertexFormat.BLOCK) {
-			// We have to fix the vertex format here, or else the vertex count will be calculated wrongly and too many
-			// vertices will be drawn.
-			//
-			// Needless to say, that is not good if you don't like access violation crashes!
 			this.format = IrisVertexFormats.TERRAIN;
 		} else if (format == DefaultVertexFormat.NEW_ENTITY) {
-			// We have to fix the vertex format here, or else the vertex count will be calculated wrongly and too many
-			// vertices will be drawn.
-			//
-			// Needless to say, that is not good if you don't like access violation crashes!
 			this.format = IrisVertexFormats.ENTITY;
 		}
 	}
