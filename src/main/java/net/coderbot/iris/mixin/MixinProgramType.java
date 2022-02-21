97e5316a834ea41118401e294cbad7b232e881c6
IMS212 <ims@ims21.tech>
2022-02-21 12:18:09 -0800

    Fix remap

--- src/main/java/net/coderbot/iris/mixin/MixinProgramType.java
+++ src/main/java/net/coderbot/iris/mixin/MixinProgramType.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(Program.Type.class)
 public class MixinProgramType {
 	@SuppressWarnings("target")
-    @Shadow(remap = false)
+    @Shadow
     @Final
     @Mutable
     private static Program.Type[] $VALUES;
