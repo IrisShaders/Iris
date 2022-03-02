f2f39da001f8e1bb6e6d7abe7c01420b32435848
coderbot <coderbot16@gmail.com>
2022-03-01 16:48:15 -0800

    GlStateManagerAccessor#getActiveTexture

--- src/main/java/net/coderbot/iris/mixin/GlStateManagerAccessor.java
+++ src/main/java/net/coderbot/iris/mixin/GlStateManagerAccessor.java
@@ -6,6 +6,11 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(GlStateManager.class)
 public interface GlStateManagerAccessor {
+	@Accessor("activeTexture")
+	static int getActiveTexture() {
+		throw new AssertionError();
+	}
+
 	@Accessor("ALPHA_TEST")
 	static GlStateManager.AlphaState getALPHA_TEST() {
 		throw new UnsupportedOperationException("Not accessed");
