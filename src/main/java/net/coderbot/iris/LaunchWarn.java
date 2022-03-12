38d0fcbb8f445bee0cd015334d5b491a03d52ae8
IMS212 <ims@ims21.tech>
2022-03-12 11:22:26 -0800

    Yes/No instead of Ok/Cancel

--- src/main/java/net/coderbot/iris/LaunchWarn.java
+++ src/main/java/net/coderbot/iris/LaunchWarn.java
@@ -9,7 +9,7 @@ public class LaunchWarn {
 	public static void main(String[] args) {
 		// TODO: make this translatable
 		String message = "This file is the Fabric version of Iris, meant to be installed as a mod. Would you like to get the Iris Installer instead?";
-		String fallback = "This file is the Fabric version of Iris, meant to be installed as a mod. Please download the Iris Installer.";
+		String fallback = "This file is the Fabric version of Iris, meant to be installed as a mod. Please download the Iris Installer from https://irisshaders.net.";
 		if (GraphicsEnvironment.isHeadless()) {
 			System.err.println(fallback);
 		} else {
@@ -20,9 +20,9 @@ public class LaunchWarn {
 			}
 
 			if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-				int option = JOptionPane.showOptionDialog(null, message, "Iris Installer", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+				int option = JOptionPane.showOptionDialog(null, message, "Iris Installer", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
 
-				if (option == JOptionPane.OK_OPTION) {
+				if (option == JOptionPane.YES_OPTION) {
 					try {
 						Desktop.getDesktop().browse(URI.create("https://irisshaders.net"));
 					} catch (IOException e) {
