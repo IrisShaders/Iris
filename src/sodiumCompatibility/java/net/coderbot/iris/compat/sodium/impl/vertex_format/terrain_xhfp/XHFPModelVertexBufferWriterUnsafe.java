f44034424a506e1ddc7e1c4503ddfbfa5461de25
IMS212 <ims@ims21.tech>
2022-03-14 18:03:11 -0700

    Unsafe Terrain XHFP

--- src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/impl/vertex_format/terrain_xhfp/XHFPModelVertexBufferWriterUnsafe.java
+++ src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/impl/vertex_format/terrain_xhfp/XHFPModelVertexBufferWriterUnsafe.java
@@ -2,22 +2,23 @@ package net.coderbot.iris.compat.sodium.impl.vertex_format.terrain_xhfp;
 
 import me.jellysquid.mods.sodium.client.model.vertex.buffer.VertexBufferView;
 import me.jellysquid.mods.sodium.client.model.vertex.buffer.VertexBufferWriterNio;
+import me.jellysquid.mods.sodium.client.model.vertex.buffer.VertexBufferWriterUnsafe;
 import me.jellysquid.mods.sodium.client.render.chunk.format.ModelVertexSink;
 import me.jellysquid.mods.sodium.client.render.chunk.format.ModelVertexUtil;
-import me.jellysquid.mods.sodium.client.util.Norm3b;
-import net.coderbot.iris.compat.sodium.impl.block_id.MaterialIdAwareVertexWriter;
 import net.coderbot.iris.block_rendering.MaterialIdHolder;
+import net.coderbot.iris.compat.sodium.impl.block_id.MaterialIdAwareVertexWriter;
 import net.coderbot.iris.compat.sodium.impl.vertex_format.IrisModelVertexFormats;
 import net.coderbot.iris.compat.sodium.impl.vertex_format.NormalHelper;
 import net.coderbot.iris.vendored.joml.Vector3f;
+import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
 
 // TODO: Add Unsafe version
-public class XHFPModelVertexBufferWriterNio extends VertexBufferWriterNio implements ModelVertexSink, MaterialIdAwareVertexWriter {
+public class XHFPModelVertexBufferWriterUnsafe extends VertexBufferWriterUnsafe implements ModelVertexSink, MaterialIdAwareVertexWriter {
     private MaterialIdHolder idHolder;
 
-    public XHFPModelVertexBufferWriterNio(VertexBufferView backingBuffer) {
+    public XHFPModelVertexBufferWriterUnsafe(VertexBufferView backingBuffer) {
         super(backingBuffer, IrisModelVertexFormats.MODEL_VERTEX_XHFP);
     }
 
@@ -27,7 +28,7 @@ public class XHFPModelVertexBufferWriterNio extends VertexBufferWriterNio implem
     float uSum;
     float vSum;
 
-    private QuadViewTerrain.QuadViewTerrainNio currentQuad = new QuadViewTerrain.QuadViewTerrainNio();
+    private QuadViewTerrain.QuadViewTerrainUnsafe currentQuad = new QuadViewTerrain.QuadViewTerrainUnsafe();
     private Vector3f normal = new Vector3f();
 
     @Override
@@ -53,25 +54,24 @@ public class XHFPModelVertexBufferWriterNio extends VertexBufferWriterNio implem
 
     private void writeQuadInternal(short x, short y, short z, int color, short u, short v, int light, short materialId,
                                    short renderType) {
-        int i = this.writeOffset;
+        long i = this.writePointer;
 
         vertexCount++;
         // NB: uSum and vSum must already be incremented outside of this function.
 
-        ByteBuffer buffer = this.byteBuffer;
-        buffer.putShort(i, x);
-        buffer.putShort(i + 2, y);
-        buffer.putShort(i + 4, z);
-        buffer.putInt(i + 8, color);
-        buffer.putShort(i + 12, u);
-        buffer.putShort(i + 14, v);
-        buffer.putInt(i + 16, light);
+        MemoryUtil.memPutShort(i, x);
+        MemoryUtil.memPutShort(i + 2, y);
+        MemoryUtil.memPutShort(i + 4, z);
+        MemoryUtil.memPutInt(i + 8, color);
+        MemoryUtil.memPutShort(i + 12, u);
+        MemoryUtil.memPutShort(i + 14, v);
+        MemoryUtil.memPutInt(i + 16, light);
 		// NB: We don't set midTexCoord, normal, and tangent here, they will be filled in later.
 		// block ID: We only set the first 2 values, any legacy shaders using z or w will get filled in based on the GLSL spec
 		// https://www.khronos.org/opengl/wiki/Vertex_Specification#Vertex_format
 		// TODO: can we pack this into one short?
-		buffer.putShort(i + 32, materialId);
-		buffer.putShort(i + 34, renderType);
+		MemoryUtil.memPutShort(i + 32, materialId);
+		MemoryUtil.memPutShort(i + 34, renderType);
 
         if (vertexCount == 4) {
             // TODO: Consider applying similar vertex coordinate transformations as the normal HFP texture coordinates
@@ -89,10 +89,10 @@ public class XHFPModelVertexBufferWriterNio extends VertexBufferWriterNio implem
             int midV = (int)(65535.0F * Math.min(vSum * 0.25f, 1.0f)) & 0xFFFF;
             int midTexCoord = (midV << 16) | midU;
 
-            buffer.putInt(i + 20, midTexCoord);
-            buffer.putInt(i + 20 - STRIDE, midTexCoord);
-            buffer.putInt(i + 20 - STRIDE * 2, midTexCoord);
-            buffer.putInt(i + 20 - STRIDE * 3, midTexCoord);
+            MemoryUtil.memPutInt(i + 20, midTexCoord);
+            MemoryUtil.memPutInt(i + 20 - STRIDE, midTexCoord);
+            MemoryUtil.memPutInt(i + 20 - STRIDE * 2, midTexCoord);
+            MemoryUtil.memPutInt(i + 20 - STRIDE * 3, midTexCoord);
 
             vertexCount = 0;
             uSum = 0;
@@ -102,21 +102,21 @@ public class XHFPModelVertexBufferWriterNio extends VertexBufferWriterNio implem
             // Implementation based on the algorithm found here:
             // https://github.com/IrisShaders/ShaderDoc/blob/master/vertex-format-extensions.md#surface-normal-vector
 
-			currentQuad.setup(buffer, writeOffset, 36);
-            NormalHelper.computeFaceNormal(normal, currentQuad);
+			currentQuad.setup(i, 36);
+			NormalHelper.computeFaceNormal(normal, currentQuad);
             int packedNormal = NormalHelper.packNormal(normal, 0.0f);
 
-            buffer.putInt(i + 28, packedNormal);
-            buffer.putInt(i + 28 - STRIDE, packedNormal);
-            buffer.putInt(i + 28 - STRIDE * 2, packedNormal);
-            buffer.putInt(i + 28 - STRIDE * 3, packedNormal);
+            MemoryUtil.memPutInt(i + 28, packedNormal);
+            MemoryUtil.memPutInt(i + 28 - STRIDE, packedNormal);
+            MemoryUtil.memPutInt(i + 28 - STRIDE * 2, packedNormal);
+            MemoryUtil.memPutInt(i + 28 - STRIDE * 3, packedNormal);
 
             int tangent = currentQuad.computeTangent(normal.x(), normal.y(), normal.z());
 
-            buffer.putInt(i + 24, tangent);
-            buffer.putInt(i + 24 - STRIDE, tangent);
-            buffer.putInt(i + 24 - STRIDE * 2, tangent);
-            buffer.putInt(i + 24 - STRIDE * 3, tangent);
+            MemoryUtil.memPutInt(i + 24, tangent);
+            MemoryUtil.memPutInt(i + 24 - STRIDE, tangent);
+            MemoryUtil.memPutInt(i + 24 - STRIDE * 2, tangent);
+            MemoryUtil.memPutInt(i + 24 - STRIDE * 3, tangent);
         }
 
         this.advance();
