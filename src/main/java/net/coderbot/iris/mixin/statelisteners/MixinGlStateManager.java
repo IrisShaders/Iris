cd4acc35722dd7dd4c8eaed9b61a439b52f73a48
IMS212 <ims@ims21.tech>
2022-03-23 09:06:24 -0700

    Fog start/end listeners and uniforms

--- src/main/java/net/coderbot/iris/mixin/statelisteners/MixinGlStateManager.java
+++ src/main/java/net/coderbot/iris/mixin/statelisteners/MixinGlStateManager.java
@@ -12,6 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinGlStateManager {
 	private static Runnable fogToggleListener;
 	private static Runnable fogModeListener;
+	private static Runnable fogStartListener;
+	private static Runnable fogEndListener;
 	private static Runnable fogDensityListener;
 	private static Runnable blendFuncListener;
 
@@ -36,6 +38,20 @@ public class MixinGlStateManager {
 		}
 	}
 
+	@Inject(method = "_fogStart(F)V", at = @At(value = "FIELD", target = "Lcom/mojang/blaze3d/platform/GlStateManager$FogState;start:F", shift = At.Shift.AFTER))
+	private static void iris$onFogStart(float density, CallbackInfo ci) {
+		if (fogStartListener != null) {
+			fogStartListener.run();
+		}
+	}
+
+	@Inject(method = "_fogEnd(F)V", at = @At(value = "FIELD", target = "Lcom/mojang/blaze3d/platform/GlStateManager$FogState;end:F", shift = At.Shift.AFTER))
+	private static void iris$onFogEnd(float density, CallbackInfo ci) {
+		if (fogEndListener != null) {
+			fogEndListener.run();
+		}
+	}
+
 	@Inject(method = "_blendFunc", at = @At("RETURN"))
 	private static void iris$onBlendFunc(int srcRgb, int dstRgb, CallbackInfo ci) {
 		if (blendFuncListener != null) {
@@ -53,6 +69,8 @@ public class MixinGlStateManager {
 	static {
 		StateUpdateNotifiers.fogToggleNotifier = listener -> fogToggleListener = listener;
 		StateUpdateNotifiers.fogModeNotifier = listener -> fogModeListener = listener;
+		StateUpdateNotifiers.fogStartNotifier = listener -> fogStartListener = listener;
+		StateUpdateNotifiers.fogEndNotifier = listener -> fogEndListener = listener;
 		StateUpdateNotifiers.fogDensityNotifier = listener -> fogDensityListener = listener;
 		StateUpdateNotifiers.blendFuncNotifier = listener -> blendFuncListener = listener;
 	}
