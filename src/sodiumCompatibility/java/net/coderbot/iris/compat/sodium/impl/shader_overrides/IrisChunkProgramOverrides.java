3e7b9066798ad5082292fe3433f15d5d307b6f19
IMS212 <ims@ims21.tech>
2022-04-25 18:36:27 -0700

    Fix shadow pass running without a shadow program

--- src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/impl/shader_overrides/IrisChunkProgramOverrides.java
+++ src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/impl/shader_overrides/IrisChunkProgramOverrides.java
@@ -163,14 +163,7 @@ public class IrisChunkProgramOverrides {
         }
     }
 
-    public void createShaders(RenderDevice device) {
-        WorldRenderingPipeline worldRenderingPipeline = Iris.getPipelineManager().getPipelineNullable();
-        SodiumTerrainPipeline sodiumTerrainPipeline = null;
-
-        if (worldRenderingPipeline != null) {
-            sodiumTerrainPipeline = worldRenderingPipeline.getSodiumTerrainPipeline();
-        }
-
+    public void createShaders(SodiumTerrainPipeline sodiumTerrainPipeline, RenderDevice device) {
         Iris.getPipelineManager().clearSodiumShaderReloadNeeded();
 
         if (sodiumTerrainPipeline != null) {
@@ -189,19 +182,24 @@ public class IrisChunkProgramOverrides {
 
     @Nullable
     public ChunkProgram getProgramOverride(RenderDevice device, BlockRenderPass pass) {
-        if (Iris.getPipelineManager().isSodiumShaderReloadNeeded()) {
+		WorldRenderingPipeline worldRenderingPipeline = Iris.getPipelineManager().getPipelineNullable();
+		SodiumTerrainPipeline sodiumTerrainPipeline = null;
+
+		if (worldRenderingPipeline != null) {
+			sodiumTerrainPipeline = worldRenderingPipeline.getSodiumTerrainPipeline();
+		}
+
+		if (Iris.getPipelineManager().isSodiumShaderReloadNeeded()) {
             deleteShaders();
-            createShaders(device);
+            createShaders(sodiumTerrainPipeline, device);
         }
 
         if (ShadowRenderingState.areShadowsCurrentlyBeingRendered()) {
-			ChunkProgram shadowProgram = this.programs.get(IrisTerrainPass.SHADOW);
-
-			if (shadowProgram == null) {
+			if (sodiumTerrainPipeline != null && !sodiumTerrainPipeline.hasShadowPass()) {
 				throw new IllegalStateException("Shadow program requested, but the pack does not have a shadow pass?");
 			}
 
-			return shadowProgram;
+			return this.programs.get(IrisTerrainPass.SHADOW);
         } else {
             return this.programs.get(pass.isTranslucent() ? IrisTerrainPass.GBUFFER_TRANSLUCENT : IrisTerrainPass.GBUFFER_SOLID);
         }
