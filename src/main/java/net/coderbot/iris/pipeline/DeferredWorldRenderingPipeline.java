68e7a93f5779de2ad9ba6735eab1b114aae04ab9
IMS212 <ims@ims21.tech>
2022-04-26 15:09:43 -0700

    Don't use an IntSupplier to refer to render target, fix viewport in ShadowRenderer

--- src/main/java/net/coderbot/iris/pipeline/DeferredWorldRenderingPipeline.java
+++ src/main/java/net/coderbot/iris/pipeline/DeferredWorldRenderingPipeline.java
@@ -754,7 +754,7 @@ public class DeferredWorldRenderingPipeline implements WorldRenderingPipeline {
 		RenderTarget main = Minecraft.getInstance().getMainRenderTarget();
 		Blaze3dRenderTargetExt mainExt = (Blaze3dRenderTargetExt) main;
 
-		renderTargets.resizeIfNeeded(mainExt.iris$isDepthBufferDirty(), main.width, main.height);
+		renderTargets.resizeIfNeeded(mainExt.iris$isDepthBufferDirty(), main.getDepthTextureId(), main.width, main.height);
 
 		mainExt.iris$clearDepthBufferDirtyFlag();
 
--- src/main/java/net/coderbot/iris/pipeline/ShadowRenderer.java
+++ src/main/java/net/coderbot/iris/pipeline/ShadowRenderer.java
@@ -451,7 +451,7 @@ public class ShadowRenderer implements ShadowMapRenderer {
 		IrisRenderSystem.restoreProjectionMatrix();
 
 		// Restore the old viewport
-		RenderSystem.viewport(0, 0, client.getWindow().getWidth(), client.getWindow().getHeight());
+		RenderSystem.viewport(0, 0, client.getMainRenderTarget().width, client.getMainRenderTarget().height);
 	}
 
 	private void copyPreTranslucentDepth() {
