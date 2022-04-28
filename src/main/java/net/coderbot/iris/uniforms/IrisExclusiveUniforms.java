511d5d9d3edc0e331e311c12879bc5638e28d8fe
IMS212 <ims@ims21.tech>
2022-04-28 08:17:35 -0700

    cameraType Iris exclusive uniform

--- src/main/java/net/coderbot/iris/uniforms/IrisExclusiveUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/IrisExclusiveUniforms.java
@@ -1,11 +1,21 @@
 package net.coderbot.iris.uniforms;
 
 import net.coderbot.iris.gl.uniform.UniformHolder;
+import net.coderbot.iris.gl.uniform.UniformUpdateFrequency;
+import net.minecraft.client.Minecraft;
 
 public class IrisExclusiveUniforms {
-
 	public static void addIrisExclusiveUniforms(UniformHolder uniforms) {
 		//All Iris-exclusive uniforms (uniforms which do not exist in either OptiFine or ShadersMod) should be registered here.
+		uniforms.uniform1i(UniformUpdateFrequency.PER_FRAME, "cameraType", IrisExclusiveUniforms::getPerspective);
 	}
 
+	private static int getPerspective() {
+		switch (Minecraft.getInstance().options.getCameraType()) {
+			case FIRST_PERSON: return 0;
+			case THIRD_PERSON_BACK: return 1;
+			case THIRD_PERSON_FRONT: return 2;
+			default: return 0;
+		}
+	}
 }
