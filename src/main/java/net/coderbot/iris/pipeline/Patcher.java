4cb5fe5c3d6c58b93d36e75cda691b1ca8a7eeea
douira <douira100@gmail.com>
2022-04-28 18:07:31 +0200

    added patch inspection for debug

--- src/main/java/net/coderbot/iris/pipeline/Patcher.java
+++ src/main/java/net/coderbot/iris/pipeline/Patcher.java
@@ -1,20 +1,36 @@
 package net.coderbot.iris.pipeline;
 
+import java.util.function.Supplier;
+
+import org.apache.logging.log4j.*;
+
 import net.coderbot.iris.gl.shader.ShaderType;
 
 public interface Patcher {
+	static boolean INSPECT_PATCH = false;
+	
 	static Patcher INSTANCE = new TransformPatcher();
 	// static Patcher INSTANCE = new AttributeShaderTransformer();
 
-	// static Logger LOGGER = LogManager.getLogger(TransformPatcher.class);
+	static Logger LOGGER = LogManager.getLogger(Patcher.class);
 
-	public default String patchAttributes(String source, ShaderType type, boolean hasGeometry) {
-		// LOGGER.debug("INPUT: " + source);
-		String patched = patchAttributesInternal(source, type, hasGeometry);
-		// LOGGER.debug("AGENT: " + getClass().getSimpleName() + " TYPE: " + type + " HAS_GEOMETRY: " + hasGeometry);
-		// LOGGER.debug("PATCHED: " + patched);
+	static String inspectPatch(String source, String patchInfo, Supplier<String> patcher) {
+		if (INSPECT_PATCH) {
+			LOGGER.debug("INPUT: " + source);
+		}
+		String patched = patcher.get();
+		if (INSPECT_PATCH) {
+			LOGGER.debug("PATCH INFO: " + patchInfo);
+			LOGGER.debug("PATCHED: " + patched);
+		}
 		return patched;
 	}
 
-	public String patchAttributesInternal(String source, ShaderType type, boolean hasGeometry);
+	default String patchAttributes(String source, ShaderType type, boolean hasGeometry) {
+		return inspectPatch(source,
+				"AGENT: " + getClass().getSimpleName() + " TYPE: " + type + "HAS_GEOMETRY: " + hasGeometry,
+				() -> patchAttributesInternal(source, type, hasGeometry));
+	}
+
+	String patchAttributesInternal(String source, ShaderType type, boolean hasGeometry);
 }
