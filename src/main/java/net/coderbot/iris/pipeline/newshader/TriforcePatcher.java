dc1e294a2abd70950d40d9d20a2299e59afaf571
coderbot <coderbot16@gmail.com>
2022-04-30 09:56:20 -0700

    fix: Work around driver bug on HD 4000 and HD 2500 hardware
    It was a simple workaround that was actually already implemented
    for Sodium terrain programs, but just not copied over to the
    other Triforce patchers.
    
    Fixes:
    - https://github.com/IrisShaders/Iris/issues/441

--- src/main/java/net/coderbot/iris/pipeline/newshader/TriforcePatcher.java
+++ src/main/java/net/coderbot/iris/pipeline/newshader/TriforcePatcher.java
@@ -266,6 +266,7 @@ public class TriforcePatcher {
 			transformations.injectLine(Transformations.InjectionPoint.BEFORE_CODE, "vec4 ftransform() { return gl_ModelViewProjectionMatrix * gl_Vertex; }");
 		}
 
+		applyIntelHd4000Workaround(transformations);
 
 		return transformations.toString();
 	}
@@ -362,8 +363,7 @@ public class TriforcePatcher {
 			transformations.injectLine(Transformations.InjectionPoint.BEFORE_CODE, "uniform mat4 u_ProjectionMatrix;");
 		}
 
-		// Just being careful
-		transformations.define("ftransform", "iris_ftransform");
+		applyIntelHd4000Workaround(transformations);
 
 		return transformations.toString();
 	}
@@ -417,7 +417,28 @@ public class TriforcePatcher {
 			transformations.injectLine(Transformations.InjectionPoint.BEFORE_CODE, "vec4 ftransform() { return gl_ModelViewProjectionMatrix * gl_Vertex; }");
 		}
 
+		applyIntelHd4000Workaround(transformations);
+
 		return transformations.toString();
+	}
+
+	private static void applyIntelHd4000Workaround(StringTransformations transformations) {
+		// This is a driver bug workaround that seems to be needed on HD 4000 and HD 2500 drivers.
+		//
+		// Without this, they will see the call to ftransform() without taking into account the fact that we've defined
+		// the function ourselves, and thus will allocate attribute location 0 to gl_Vertex since the driver thinks that
+		// we are referencing gl_Vertex by calling the compatibility-profile ftransform() function - despite using a
+		// core profile shader version where that function does not exist.
+		//
+		// Then, when we try to bind the attribute locations for our vertex format, the shader will fail to link.
+		// By renaming the function, we avoid the driver bug here, since it no longer thinks that we're trying
+		// to call the compatibility profile fransform() function.
+		//
+		// See: https://github.com/IrisShaders/Iris/issues/441
+		//
+		// Note that this happens after we've added our ftransform function - so that both all the calls and our
+		// function are renamed in one go.
+		transformations.define("ftransform", "iris_ftransform");
 	}
 
 	private static void addAlphaTest(StringTransformations transformations, ShaderType type, AlphaTest alpha) {
