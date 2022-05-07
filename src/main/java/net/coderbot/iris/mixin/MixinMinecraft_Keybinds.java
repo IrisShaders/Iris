a2b13551af18df71912182829d020acc209176e7
coderbot <coderbot16@gmail.com>
2022-05-07 16:48:58 -0700

    Suppress the authentication check in development environments

--- src/main/java/net/coderbot/iris/mixin/MixinMinecraft_Keybinds.java
+++ src/main/java/net/coderbot/iris/mixin/MixinMinecraft_Keybinds.java
@@ -2,9 +2,7 @@ package net.coderbot.iris.mixin;
 
 import net.coderbot.iris.Iris;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.util.profiling.ProfilerFiller;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  * alone shaves over 60kB off the released JAR size.</p>
  */
 @Mixin(Minecraft.class)
-public class MixinMinecraft {
+public class MixinMinecraft_Keybinds {
 	@Shadow
 	private ProfilerFiller profiler;
 
