b9b4e0c147cc43ee470661834e83a3a81b774ccb
coderbot <coderbot16@gmail.com>
2022-05-08 17:52:00 -0700

    Fix log spam and errors in properties preprocessing
    Fixes:
    - https://github.com/IrisShaders/Iris/issues/1295

--- src/test/java/net/coderbot/iris/test/shaderpack/PreprocessorTest.java
+++ src/test/java/net/coderbot/iris/test/shaderpack/PreprocessorTest.java
@@ -19,4 +19,46 @@ public class PreprocessorTest {
 		Assertions.assertEquals("Test Test\n", PropertiesPreprocessor.preprocessSource("Test \\\nTest",
 			IrisTests.TEST_ENVIRONMENT_DEFINES));
 	}
+
+	@Test
+	void testWeirdHashComment() {
+		String line = "test= <empty> #[Comment]";
+
+		// This needs to be handled properly for Oceano shaders to work right
+		Assertions.assertEquals(line + "\n", PropertiesPreprocessor.preprocessSource(line,
+			IrisTests.TEST_ENVIRONMENT_DEFINES));
+	}
+
+	@Test
+	void testHashComment() {
+		// Note: Verify that this does not spam the log by inspecting manually.
+		String line =
+			"# Test\n" +
+			"#test comment\n" +
+			"#1xxx\n";
+
+		Assertions.assertEquals("", PropertiesPreprocessor.preprocessSource(line,
+			IrisTests.TEST_ENVIRONMENT_DEFINES).trim());
+	}
+
+	@Test
+	void testPreprocessorDirectives() {
+		String line =
+			"#if MC_VERSION >= 11300\n" +
+			"block.1=minecraft:grass_block\n" +
+			"#else\n" +
+			"block.1=minecraft:grass\n" +
+			"#endif\n";
+
+		Assertions.assertEquals("block.1=minecraft:grass_block", PropertiesPreprocessor.preprocessSource(line,
+			IrisTests.TEST_ENVIRONMENT_DEFINES).trim());
+	}
+
+	@Test
+	void testWeirdDefineDoesNotCrash() {
+		String line = "# define TEST";
+
+		Assertions.assertEquals("", PropertiesPreprocessor.preprocessSource(line,
+			IrisTests.TEST_ENVIRONMENT_DEFINES).trim());
+	}
 }
