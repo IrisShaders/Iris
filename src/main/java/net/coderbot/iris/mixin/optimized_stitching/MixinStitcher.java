811d42d876c205f1e092ce853674b33184dbda0b
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2022-05-08 23:01:39 -0700

    Update MixinStitcher comments

--- src/main/java/net/coderbot/iris/mixin/optimized_stitching/MixinStitcher.java
+++ src/main/java/net/coderbot/iris/mixin/optimized_stitching/MixinStitcher.java
@@ -44,7 +44,14 @@ public class MixinStitcher {
 			return false;
 		}
 
+		// Iris start
 		boolean growWidth;
+		/*
+		 * Iris short-circuits the logic if only one dimension can fit,
+		 * but vanilla does not. This can potentially lead to maxHeight
+		 * not being respected. Plus, doing it this way makes the code
+		 * easier to understand.
+		 */
 		if (canFitWidth & canFitHeight) {
 			// Effective size calculation moved from head to be inside if block
 			int effectiveWidth = Mth.smallestEncompassingPowerOfTwo(storageX);
@@ -53,6 +60,26 @@ public class MixinStitcher {
 			boolean wouldGrowEffectiveHeight = effectiveHeight != newEffectiveHeight;
 
 			if (wouldGrowEffectiveWidth) {
+				/*
+				 * The logic here differs from vanilla slightly as it combines the
+				 * vanilla checks together into the if statement.
+				 *
+				 * If the effective height would not be grown:
+				 *   Under the same conditions, vanilla would grow the width instead
+				 *   of the height. This is inefficient because it can potentially
+				 *   increase the atlas width, which is (usually) unnecessary since
+				 *   there is already enough free space on the bottom to not grow the
+				 *   atlas size.
+				 *
+				 * If the effective height would be grown but the width is greater
+				 * than the height:
+				 *   Under the same conditions, vanilla would always grow the height.
+				 *   However, not performing the additional check would cause some
+				 *   edge cases to result in cut-off sprites. The following case is
+				 *   one such example.
+				 *   1. The first sprite is added. Its dimensions measure 128x32.
+				 *   2. The second sprite is added. Its dimensions measure 256x16.
+				 */
 				if (wouldGrowEffectiveHeight && effectiveWidth <= effectiveHeight) {
 					/*
 					 * If both the width and height would be grown, definitively grow the
@@ -61,6 +88,10 @@ public class MixinStitcher {
 					growWidth = true;
 				} else {
 					/*
+					 * At this point, the height should be grown to maximize atlas space
+					 * usage, but this is not always possible to do. Hence, the following
+					 * check is employed to ensure that the height can actually be grown.
+					 *
 					 * If the height is grown, the new region's width is equal to the total
 					 * width. However, if the holder's width is larger than the total width,
 					 * the new region will not be able to contain it. Therefore, the width
@@ -76,6 +107,8 @@ public class MixinStitcher {
 					 * A similar check does not have to be performed for the heights due to
 					 * the sprite sorting - taller sprites are always added before shorter
 					 * ones.
+					 *
+					 * Vanilla does not perform this check.
 					 */
 					growWidth = holder.width > storageX;
 				}
@@ -84,6 +117,12 @@ public class MixinStitcher {
 					/*
 					 * If the height would be grown but the width would not be, grow the
 					 * width to fill up the unused space on the right.
+					 *
+					 * Under the same conditions, vanilla would grow the height instead
+					 * of the width. This is inefficient because it can potentially
+					 * increase the atlas height, which is unnecessary since there is
+					 * already enough free space on the right to not grow the atlas
+					 * size.
 					 */
 					growWidth = true;
 				} else {
@@ -98,6 +137,7 @@ public class MixinStitcher {
 		} else {
 			growWidth = canFitWidth;
 		}
+		// Iris end
 
 		Region region;
 		if (growWidth) {
