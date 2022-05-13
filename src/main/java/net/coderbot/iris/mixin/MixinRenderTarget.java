69b6599fb5f00e5daccfe738cd7e9fb72238f266
IMS <ims@ims21.tech>
2022-05-12 19:45:52 -0700

    remove modifyarg

--- src/main/java/net/coderbot/iris/mixin/MixinRenderTarget.java
+++ src/main/java/net/coderbot/iris/mixin/MixinRenderTarget.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
