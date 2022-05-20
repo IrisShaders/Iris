70876744201c0e9b45026e9b916a96807e24b02f
IMS212 <ims@ims21.tech>
2022-05-20 09:15:47 -0700

    fix everything i blew up

--- src/main/java/net/coderbot/iris/pipeline/newshader/NewWorldRenderingPipeline.java
+++ src/main/java/net/coderbot/iris/pipeline/newshader/NewWorldRenderingPipeline.java
@@ -330,6 +330,7 @@ public class NewWorldRenderingPipeline implements WorldRenderingPipeline, CoreWo
 		BlockRenderingSettings.INSTANCE.setAmbientOcclusionLevel(programSet.getPackDirectives().getAmbientOcclusionLevel());
 		BlockRenderingSettings.INSTANCE.setDisableDirectionalShading(shouldDisableDirectionalShading());
 		BlockRenderingSettings.INSTANCE.setUseSeparateAo(programSet.getPackDirectives().shouldUseSeparateAo());
+		BlockRenderingSettings.INSTANCE.setUseExtendedVertexFormat(true);
 
 		this.clearPassesFull = ClearPassCreator.createClearPasses(renderTargets, true,
 				programSet.getPackDirectives().getRenderTargetDirectives());
