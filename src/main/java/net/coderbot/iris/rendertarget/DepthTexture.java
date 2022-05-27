a7401c9538532df6c627e5645ef14134944ceefc
IMS212 <ims@ims21.tech>
2022-05-27 07:13:22 -0700

    Remove glClearTexImage (not needed)

--- src/main/java/net/coderbot/iris/rendertarget/DepthTexture.java
+++ src/main/java/net/coderbot/iris/rendertarget/DepthTexture.java
@@ -9,12 +9,9 @@ import net.coderbot.iris.gl.texture.DepthBufferFormat;
 import org.lwjgl.opengl.GL11C;
 import org.lwjgl.opengl.GL13C;
 import org.lwjgl.opengl.GL20C;
-import org.lwjgl.opengl.GL44C;
-
-import java.nio.ByteBuffer;
 
 public class DepthTexture extends GlResource {
-	public DepthTexture(int width, int height, GlFramebuffer depthTexture, DepthBufferFormat format) {
+	public DepthTexture(int width, int height, GlFramebuffer copyTexture, DepthBufferFormat format) {
 		super(GlStateManager._genTexture());
 		GlStateManager._bindTexture(getGlId());
 
@@ -22,23 +19,21 @@ public class DepthTexture extends GlResource {
 		RenderSystem.texParameter(GL11C.GL_TEXTURE_2D, GL11C.GL_TEXTURE_MAG_FILTER, GL11C.GL_NEAREST);
 		RenderSystem.texParameter(GL11C.GL_TEXTURE_2D, GL11C.GL_TEXTURE_WRAP_S, GL13C.GL_CLAMP_TO_EDGE);
 		RenderSystem.texParameter(GL11C.GL_TEXTURE_2D, GL11C.GL_TEXTURE_WRAP_T, GL13C.GL_CLAMP_TO_EDGE);
-		resize(width, height, depthTexture, format);
+		resize(width, height, copyTexture, format);
 
 		GlStateManager._bindTexture(0);
 	}
 
-	void resize(int width, int height, GlFramebuffer depthTexture, DepthBufferFormat format) {
+	void resize(int width, int height, GlFramebuffer copyTexture, DepthBufferFormat format) {
 		GlStateManager._bindTexture(getGlId());
 
-		if (depthTexture != null) {
-			depthTexture.bindAsReadBuffer();
+		if (copyTexture != null) {
+			copyTexture.bindAsReadBuffer();
 			IrisRenderSystem.copyTexImage2D(GL20C.GL_TEXTURE_2D, 0, format.getGlInternalFormat(), 0, 0, width, height, 0);
 		} else {
 			GlStateManager._texImage2D(GL11C.GL_TEXTURE_2D, 0, format.getGlInternalFormat(), width, height, 0,
 				format.getGlType(), format.getGlFormat(), null);
 		}
-
-		GL44C.glClearTexImage(getTextureId(), 0, format.getGlType(), format.getGlFormat(), (ByteBuffer) null);
 
 		GlStateManager._bindTexture(0);
 	}
