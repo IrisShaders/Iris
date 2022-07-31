f982c5edf1e0fa9c4b728e1157fbbec3424501d7
douira <douira100@gmail.com>
2022-07-31 17:29:13 +0200

    update equals method

--- src/main/java/net/coderbot/iris/pipeline/transform/Parameters.java
+++ src/main/java/net/coderbot/iris/pipeline/transform/Parameters.java
@@ -25,6 +25,8 @@ class Parameters extends JobParameters {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
+		if (obj == null)
+			return false;
 		if (getClass() != obj.getClass())
 			return false;
 		Parameters other = (Parameters) obj;
