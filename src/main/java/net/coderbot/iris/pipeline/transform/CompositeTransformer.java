91341338dbbb6a8f689d036ed3e255a67f317fa0
douira <douira100@gmail.com>
2022-08-02 02:20:39 +0200

    separate composite depth transformer

--- src/main/java/net/coderbot/iris/pipeline/transform/CompositeTransformer.java
+++ src/main/java/net/coderbot/iris/pipeline/transform/CompositeTransformer.java
@@ -3,32 +3,17 @@ package net.coderbot.iris.pipeline.transform;
 import java.util.stream.Stream;
 
 import io.github.douira.glsl_transformer.ast.node.TranslationUnit;
-import io.github.douira.glsl_transformer.ast.node.basic.ASTNode;
 import io.github.douira.glsl_transformer.ast.node.expression.unary.FunctionCallExpression;
-import io.github.douira.glsl_transformer.ast.node.external_declaration.ExternalDeclaration;
 import io.github.douira.glsl_transformer.ast.query.Root;
-import io.github.douira.glsl_transformer.ast.query.match.AutoHintedMatcher;
-import io.github.douira.glsl_transformer.ast.query.match.Matcher;
 import io.github.douira.glsl_transformer.ast.transform.ASTInjectionPoint;
 import io.github.douira.glsl_transformer.ast.transform.ASTTransformer;
 
 class CompositeTransformer {
-	private static final AutoHintedMatcher<ExternalDeclaration> uniformFloatCenterDepthSmooth = new AutoHintedMatcher<>(
-			"uniform float centerDepthSmooth;", Matcher.externalDeclarationPattern);
-
 	public static void transform(
 			ASTTransformer<?> t,
 			TranslationUnit tree,
 			Root root) {
-		// replace original declaration
-		if (root.processMatches(t, uniformFloatCenterDepthSmooth, ASTNode::detachAndDelete)) {
-			tree.parseAndInjectNode(t, ASTInjectionPoint.BEFORE_DECLARATIONS,
-					"uniform sampler2D iris_centerDepthSmooth;");
-
-			// if centerDepthSmooth is not declared as a uniform, we don't make it available
-			root.replaceReferenceExpressions(t, "centerDepthSmooth",
-					"texture2D(iris_centerDepthSmooth, vec2(0.5)).r");
-		}
+		CompositeDepthTransformer.transform(t, tree, root);
 
 		// if using a lod texture sampler and on version 120, patch in the extension
 		// #extension GL_ARB_shader_texture_lod : require
