1888be376dd7724266ffd6afbe2136aad86d3763
douira <douira100@gmail.com>
2022-08-17 03:49:06 +0200

    compat patcher with declaration thing

--- src/main/java/net/coderbot/iris/pipeline/transform/CompatibilityTransformer.java
+++ src/main/java/net/coderbot/iris/pipeline/transform/CompatibilityTransformer.java
@@ -1,8 +1,9 @@
 package net.coderbot.iris.pipeline.transform;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,9 +12,11 @@ import io.github.douira.glsl_transformer.ast.node.TranslationUnit;
 import io.github.douira.glsl_transformer.ast.node.basic.ASTNode;
 import io.github.douira.glsl_transformer.ast.node.declaration.Declaration;
 import io.github.douira.glsl_transformer.ast.node.declaration.TypeAndInitDeclaration;
+import io.github.douira.glsl_transformer.ast.node.expression.LiteralExpression;
 import io.github.douira.glsl_transformer.ast.node.external_declaration.DeclarationExternalDeclaration;
 import io.github.douira.glsl_transformer.ast.node.external_declaration.EmptyDeclaration;
 import io.github.douira.glsl_transformer.ast.node.external_declaration.ExternalDeclaration;
+import io.github.douira.glsl_transformer.ast.node.external_declaration.FunctionDefinition;
 import io.github.douira.glsl_transformer.ast.node.statement.terminal.DeclarationStatement;
 import io.github.douira.glsl_transformer.ast.node.type.qualifier.StorageQualifier;
 import io.github.douira.glsl_transformer.ast.node.type.qualifier.TypeQualifier;
@@ -106,61 +109,115 @@ public class CompatibilityTransformer {
 	private static final String typeTag = tag + "1";
 	private static final String nameTag = tag + "2";
 	private static final String outDeclarationTemplate = "out " + typeTag + " " + nameTag + ";";
+	private static final String initTemplate = nameTag + " = " + typeTag + ";";
 
 	// does transformations that require cross-shader type data
 	public static void transformGrouped(
 			ASTParser t,
 			Map<PatchShaderType, TranslationUnit> trees,
 			Parameters parameters) {
-		// TODO: improve this when there is node cloning support in glsl-transformer
-		// find attributes that are declared as "in" in geometry or fragment but not
-		// declared as "out" in the previous stage. The missing "out" declarations for
-		// these attributes are added and initialized.
-		for (int i = 1; i < pipeline.length; i++) {
-			ShaderType prevType = pipeline[i - 1];
+		/**
+		 * find attributes that are declared as "in" in geometry or fragment but not
+		 * declared as "out" in the previous stage. The missing "out" declarations for
+		 * these attributes are added and initialized.
+		 * TODO:
+		 * - improve this when there is node cloning support in glsl-transformer
+		 * - fix issues where Iris' own declarations are detected and patched like
+		 * iris_FogFragCoord if there are geometry shaders present
+		 * - improved geometry shader support? They use funky declarations
+		 */
+		ShaderType prevType = null;
+		for (int i = 0; i < pipeline.length; i++) {
 			ShaderType type = pipeline[i];
-
-			// the fragment shader has to be processed twice
-			PatchShaderType prevPatchTypes = PatchShaderType.fromGlShaderType(prevType)[0];
 			PatchShaderType[] patchTypes = PatchShaderType.fromGlShaderType(type);
+
+			// check if the patch types have sources and continue if not
+			boolean hasAny = false;
+			for (PatchShaderType currentType : patchTypes) {
+				if (trees.get(currentType) != null) {
+					hasAny = true;
+				}
+			}
+			if (!hasAny) {
+				continue;
+			}
+
+			// if the current type has sources but the previous one doesn't, set the
+			// previous one and continue
+			if (prevType == null) {
+				prevType = type;
+				continue;
+			}
+
+			PatchShaderType prevPatchTypes = PatchShaderType.fromGlShaderType(prevType)[0];
 			TranslationUnit prevTree = trees.get(prevPatchTypes);
 			Root prevRoot = prevTree.getRoot();
 
 			// find out declarations
-			var outDeclarations = new HashMap<>();
+			Set<String> outDeclarations = new HashSet<String>();
 			for (ExternalDeclaration declaration : prevRoot.nodeIndex.get(DeclarationExternalDeclaration.class)) {
 				if (outDeclarationMatcher.matchesExtract(declaration)) {
-					outDeclarations.put(
-							outDeclarationMatcher.getStringDataMatch("name"),
-							outDeclarationMatcher.getNodeMatch("type", TypeSpecifier.class));
+					outDeclarations.add(outDeclarationMatcher.getStringDataMatch("name"));
 				}
 			}
 
 			// add out declarations that are missing for in declarations
 			for (PatchShaderType currentType : patchTypes) {
 				TranslationUnit tree = trees.get(currentType);
+				if (tree == null) {
+					continue;
+				}
 				Root root = tree.getRoot();
+
+				// find the main function
+				var mainFunction = prevRoot.identifierIndex.getStream("main")
+						.map(id -> id.getBranchAncestor(FunctionDefinition.class, FunctionDefinition::getFunctionPrototype))
+						.filter(Objects::nonNull).findAny();
+				if (!mainFunction.isPresent()) {
+					LOGGER.warn(
+							"A shader is missing a main function and could not be compatibility-patched.");
+					continue;
+				}
+				var mainFunctionStatements = mainFunction.get().getBody();
+
 				for (ExternalDeclaration declaration : root.nodeIndex.get(DeclarationExternalDeclaration.class)) {
 					if (inDeclarationMatcher.matchesExtract(declaration)) {
 						var name = inDeclarationMatcher.getStringDataMatch("name");
 						var specifier = inDeclarationMatcher.getNodeMatch("type", BuiltinNumericTypeSpecifier.class);
 
-						if (specifier != null && !outDeclarations.containsKey(name)) {
-							var inDeclaration = t.parseExternalDeclaration(tree, outDeclarationTemplate);
-							tree.injectNode(ASTInjectionPoint.BEFORE_DECLARATIONS, inDeclaration);
-							root.identifierIndex.rename(nameTag, name);
+						if (!outDeclarations.contains(name)) {
+							if (specifier == null) {
+								LOGGER.warn(
+										"The in declaration " + name + " in the " + currentType.glShaderType.name()
+												+ " shader that has a missing corresponding out declaration in the previous stage "
+												+ prevType.name() + " has a non-numeric type and could not be compatibility-patched.");
+							}
+							var inDeclaration = t.parseExternalDeclaration(prevTree, outDeclarationTemplate);
+							prevTree.injectNode(ASTInjectionPoint.BEFORE_DECLARATIONS, inDeclaration);
+							// rename happens later
 
-							// TODO: more efficient copying of the fully specified type
-							// use node cloning once available
-							root.identifierIndex.getOne(typeTag).getAncestor(TypeSpecifier.class)
-									.replaceBy(new BuiltinNumericTypeSpecifier(specifier.type));
+							prevRoot.identifierIndex.getOne(typeTag).getAncestor(TypeSpecifier.class)
+									.replaceByAndDelete(new BuiltinNumericTypeSpecifier(specifier.type));
+
+							var init = t.parseStatement(prevTree, initTemplate);
+							mainFunctionStatements.getChildren().add(0, init);
+							prevRoot.identifierIndex.rename(nameTag, name);
+							prevRoot.identifierIndex.getOneReferenceExpression(typeTag)
+									.replaceByAndDelete(LiteralExpression.getDefaultValue(specifier.type));
+
+							LOGGER.warn(
+									"The in declaration " + name + " in the " + currentType.glShaderType.name()
+											+ " shader that has a missing corresponding out declaration in the previous stage "
+											+ prevType.name() + " and has been compatibility-patched.");
+
+							// update out declarations to prevent duplicates
+							outDeclarations.add(name);
 						}
 					}
 				}
-
-				// TODO: add a initialization in the main function
-				// LiteralExpression.getDefaultValue(numberType)
 			}
+
+			prevType = type;
 		}
 	}
 }
