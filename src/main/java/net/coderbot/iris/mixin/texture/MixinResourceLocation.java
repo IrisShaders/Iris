18b432385fa1011db2659edf0ccab27f6e8fff1c
IMS212 <ims@ims21.tech>
2022-08-22 10:10:54 -0700

    Allow broken paths

---
+++ src/main/java/net/coderbot/iris/mixin/texture/MixinResourceLocation.java
@@ -0,0 +1,22 @@
+package net.coderbot.iris.mixin.texture;
+
+import net.coderbot.iris.Iris;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ResourceLocation.class)
+public class MixinResourceLocation {
+	@Inject(method = "isValidPath", at = @At("HEAD"), cancellable = true)
+	private static void iris$allowBrokenPaths(String string, CallbackInfoReturnable<Boolean> cir) {
+		for (int i = 0; i < string.length(); ++i) {
+			if (ResourceLocation.validPathChar(string.charAt(i))) continue;
+			Iris.logger.warn("Path " + string + " is invalid. Iris allows this behavior, however the pack developer should fix this!");
+			break;
+		}
+
+		cir.setReturnValue(true);
+	}
+}
