53513687d2f4ddcebfa9c43469c19fdc16a585fb
IMS212 <ims@ims21.tech>
2022-08-23 11:18:15 -0700

    DUMMY

--- src/main/java/net/coderbot/iris/mixin/texture/MixinResourceLocation.java
+++ src/main/java/net/coderbot/iris/mixin/texture/MixinResourceLocation.java
@@ -11,6 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinResourceLocation {
 	@Inject(method = "isValidPath", at = @At("HEAD"), cancellable = true)
 	private static void iris$allowBrokenPaths(String string, CallbackInfoReturnable<Boolean> cir) {
+		if (string.equals("DUMMY")) {
+			// This is here to solve a weird case in DFU that expects minecraft:DUMMY to be invalid.
+			cir.setReturnValue(false);
+			return;
+		}
+
 		for (int i = 0; i < string.length(); ++i) {
 			if (ResourceLocation.validPathChar(string.charAt(i))) continue;
 			Iris.logger.warn("Path " + string + " is invalid. Iris allows this behavior, however the pack developer should fix this!");
