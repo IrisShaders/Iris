f94c308b910cc8b2d3c81ea3389915b8040cdad8
IMS212 <ims@ims21.tech>
2022-09-03 15:01:08 -0700

    Refractor to support old hand light

--- src/main/java/net/coderbot/iris/uniforms/CommonUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/CommonUniforms.java
@@ -63,7 +63,7 @@ public final class CommonUniforms {
 		WorldTimeUniforms.addWorldTimeUniforms(uniforms);
 		SystemTimeUniforms.addSystemTimeUniforms(uniforms);
 		new CelestialUniforms(directives.getSunPathRotation()).addCelestialUniforms(uniforms);
-		IdMapUniforms.addIdMapUniforms(uniforms, idMap);
+		IdMapUniforms.addIdMapUniforms(updateNotifier, uniforms, idMap, directives.isOldHandLight());
 		IrisExclusiveUniforms.addIrisExclusiveUniforms(uniforms);
 		MatrixUniforms.addMatrixUniforms(uniforms, directives);
 		HardcodedCustomUniforms.addHardcodedCustomUniforms(uniforms, updateNotifier);
@@ -108,8 +108,6 @@ public final class CommonUniforms {
 			.uniform1f(PER_FRAME, "eyeAltitude", () -> Objects.requireNonNull(client.getCameraEntity()).getEyeY())
 			.uniform1i(PER_FRAME, "isEyeInWater", CommonUniforms::isEyeInWater)
 			.uniform1f(PER_FRAME, "blindness", CommonUniforms::getBlindness)
-			.uniform1i(PER_FRAME, "heldBlockLightValue", new HeldItemLightingSupplier(InteractionHand.MAIN_HAND))
-			.uniform1i(PER_FRAME, "heldBlockLightValue2", new HeldItemLightingSupplier(InteractionHand.OFF_HAND))
 			.uniform1f(PER_FRAME, "nightVision", CommonUniforms::getNightVision)
 			// TODO: Do we need to clamp this to avoid fullbright breaking shaders? Or should shaders be able to detect
 			//       that the player is trying to turn on fullbright?
@@ -247,30 +245,6 @@ public final class CommonUniforms {
 			return 2;
 		} else {
 			return 0;
-		}
-	}
-
-	private static class HeldItemLightingSupplier implements IntSupplier {
-
-		private final InteractionHand hand;
-
-		private HeldItemLightingSupplier(InteractionHand targetHand) {
-			this.hand = targetHand;
-		}
-
-		@Override
-		public int getAsInt() {
-			if (client.player == null) {
-				return 0;
-			}
-
-			ItemStack stack = client.player.getItemInHand(hand);
-
-			if (stack == ItemStack.EMPTY || stack == null) {
-				return 0;
-			}
-
-			return ((IrisItemLightProvider)stack.getItem()).getLightEmission(client.player, stack);
 		}
 	}
 
