55b91a6b9f8ef3815d79d57758acbd2cfa3733a6
IMS212 <ims@ims21.tech>
2022-09-10 13:42:04 -0700

    Run keybind check after FAPI

--- src/main/java/net/coderbot/iris/mixin/MixinKeyMapping.java
+++ src/main/java/net/coderbot/iris/mixin/MixinKeyMapping.java
@@ -13,9 +13,9 @@ import java.util.Optional;
 
 /**
  * Maps the key binding categories for Iris to the {@code CATEGORY_SORT_ORDER}, to allow comparisons to work correctly.
- * This is done before Fabric API.
+ * This is done after Fabric API.
  */
-@Mixin(value = KeyMapping.class, priority = 999)
+@Mixin(value = KeyMapping.class, priority = 1010)
 public class MixinKeyMapping {
 	@Shadow
 	@Final
