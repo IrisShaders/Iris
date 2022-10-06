091fb7d0af11a5313666133d495063feacae155a
IMS212 <ims@ims21.tech>
2022-10-05 19:22:11 -0700

    Fix sky alpha test

--- src/main/java/net/coderbot/iris/pipeline/newshader/ShaderKey.java
+++ src/main/java/net/coderbot/iris/pipeline/newshader/ShaderKey.java
@@ -16,7 +16,7 @@ public enum ShaderKey {
 	TEXTURED               (ProgramId.Textured,    AlphaTests.NON_ZERO_ALPHA,  DefaultVertexFormat.POSITION_TEX,                FogMode.OFF,          LightingModel.LIGHTMAP  ),
 	TEXTURED_COLOR         (ProgramId.Textured,    AlphaTests.ONE_TENTH_ALPHA, DefaultVertexFormat.POSITION_TEX_COLOR,          FogMode.OFF,          LightingModel.LIGHTMAP  ),
 	SKY_BASIC              (ProgramId.SkyBasic,    AlphaTests.OFF,             DefaultVertexFormat.POSITION,                    FogMode.PER_VERTEX,   LightingModel.LIGHTMAP  ),
-	SKY_BASIC_COLOR        (ProgramId.SkyBasic,    AlphaTests.ONE_TENTH_ALPHA, DefaultVertexFormat.POSITION_COLOR,              FogMode.OFF,          LightingModel.LIGHTMAP  ),
+	SKY_BASIC_COLOR        (ProgramId.SkyBasic,    AlphaTests.NON_ZERO_ALPHA,  DefaultVertexFormat.POSITION_COLOR,              FogMode.OFF,          LightingModel.LIGHTMAP  ),
 	SKY_TEXTURED           (ProgramId.SkyTextured, AlphaTests.OFF,             DefaultVertexFormat.POSITION_TEX,                FogMode.OFF,          LightingModel.LIGHTMAP  ),
 	SKY_TEXTURED_COLOR     (ProgramId.SkyTextured, AlphaTests.OFF,             DefaultVertexFormat.POSITION_TEX_COLOR,          FogMode.OFF,          LightingModel.LIGHTMAP  ),
 	CLOUDS                 (ProgramId.Clouds,      AlphaTests.ONE_TENTH_ALPHA, DefaultVertexFormat.POSITION_TEX_COLOR_NORMAL,   FogMode.PER_VERTEX,   LightingModel.LIGHTMAP  ),
