98eba0f02eb20147f4f1b3062e30f53aa0df8b49
IMS212 <ims@irisshaders.dev>
2022-11-18 18:11:16 -0800

    Port Vanilla Max Shadow Distance slider
    It only took half a year

--- src/main/java/net/coderbot/iris/gui/option/IrisVideoSettings.java
+++ src/main/java/net/coderbot/iris/gui/option/IrisVideoSettings.java
@@ -1,11 +1,16 @@
 package net.coderbot.iris.gui.option;
 
+import com.mojang.serialization.Codec;
 import net.coderbot.iris.Iris;
 import net.coderbot.iris.pipeline.WorldRenderingPipeline;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.OptionInstance;
+import net.minecraft.client.Options;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 
 import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.util.Mth;
 
 import java.io.IOException;
 import java.util.List;
@@ -29,8 +34,52 @@ public class IrisVideoSettings {
 				.orElse(true);
 	}
 	// TODO 22w12a fix this
-/*
-	public static final ProgressOption RENDER_DISTANCE = new ShadowDistanceOption("options.iris.shadowDistance", 0.0D, 32.0D, 1.0F, (gameOptions) -> {
+
+	public static final OptionInstance<Integer> RENDER_DISTANCE = new ShadowDistanceOption<>("options.renderDistance",
+		mc -> {
+			WorldRenderingPipeline pipeline = Iris.getPipelineManager().getPipelineNullable();
+
+			Component tooltip;
+
+			if (pipeline != null) {
+				if (pipeline.getForcedShadowRenderDistanceChunksForDisplay().isPresent()) {
+					tooltip = DISABLED_TOOLTIP;
+				} else {
+					tooltip = ENABLED_TOOLTIP;
+				}
+			} else {
+				tooltip = ENABLED_TOOLTIP;
+			}
+
+			return aDouble -> Minecraft.getInstance().font.split(tooltip, 200);
+		},
+		(arg, d) -> {
+			WorldRenderingPipeline pipeline = Iris.getPipelineManager().getPipelineNullable();
+
+			if (pipeline != null) {
+				d = pipeline.getForcedShadowRenderDistanceChunksForDisplay().orElse(d);
+			}
+
+			if (d <= 0.0) {
+				return Component.translatable("options.generic_value", Component.translatable("options.iris.shadowDistance"), "0 (disabled)");
+			} else {
+				return Component.translatable("options.generic_value",
+					Component.translatable("options.iris.shadowDistance"),
+					Component.translatable("options.chunks", d));
+			}
+		},
+		new OptionInstance.IntRange(0, 32),
+		getOverriddenShadowDistance(shadowDistance),
+		integer -> {
+			shadowDistance = integer;
+			try {
+				Iris.getIrisConfig().save();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+	/* new ShadowDistanceOption("options.iris.shadowDistance", 0.0D, 32.0D, 1.0F, (gameOptions) -> {
 		return (double) getOverriddenShadowDistance(shadowDistance);
 	}, (gameOptions, viewDistance) -> {
 		double outputShadowDistance = viewDistance;
--- src/main/java/net/coderbot/iris/mixin/gui/MixinVideoSettingsScreen.java
+++ src/main/java/net/coderbot/iris/mixin/gui/MixinVideoSettingsScreen.java
@@ -28,9 +28,10 @@ public abstract class MixinVideoSettingsScreen extends Screen {
 			index = 0
 	)
 	private OptionInstance<?>[] iris$addShaderPackScreenButton(OptionInstance<?>[] $$0) {
-		OptionInstance[] options = new OptionInstance[$$0.length + 1];
+		OptionInstance<?>[] options = new OptionInstance[$$0.length + 2];
 		System.arraycopy($$0, 0, options, 0, $$0.length);
-		options[options.length - 1] = new OptionInstance<>("options.iris.shaderPackSelection", OptionInstance.cachedConstantTooltip(Component.empty()), (arg, object) -> Component.empty(), OptionInstance.BOOLEAN_VALUES, true, (parent) -> minecraft.setScreen(new ShaderPackScreen(this)));
+		options[options.length - 2] = new OptionInstance<>("options.iris.shaderPackSelection", OptionInstance.cachedConstantTooltip(Component.empty()), (arg, object) -> Component.empty(), OptionInstance.BOOLEAN_VALUES, true, (parent) -> minecraft.setScreen(new ShaderPackScreen(this)));
+		options[options.length - 1] = IrisVideoSettings.RENDER_DISTANCE;
 		return options;
 	}
 }
