d39d0ede1e1ffa74fb8be3f00d9a47a3929c72f1
IMS212 <ims@irisshaders.dev>
2023-01-24 18:55:13 -0800

    fix: more leaky state

--- src/main/java/net/coderbot/iris/mixin/MixinLevelRenderer.java
+++ src/main/java/net/coderbot/iris/mixin/MixinLevelRenderer.java
@@ -90,6 +90,7 @@ public class MixinLevelRenderer {
 		HandRenderer.INSTANCE.renderTranslucent(poseStack, tickDelta, camera, gameRenderer, pipeline);
 		Minecraft.getInstance().getProfiler().popPush("iris_final");
 		pipeline.finalizeLevelRendering();
+		RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
 		pipeline = null;
 	}
 
