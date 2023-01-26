49c853559f513f818a537adc5da2817343d0bb43
IMS212 <ims@irisshaders.dev>
2023-01-26 09:11:15 -0800

    Fix trims disappearing due to bad render type

---
+++ src/main/java/net/coderbot/iris/mixin/MixinSheets.java
@@ -0,0 +1,24 @@
+package net.coderbot.iris.mixin;
+
+import net.coderbot.batchedentityrendering.impl.BlendingStateHolder;
+import net.coderbot.batchedentityrendering.impl.TransparencyType;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.Sheets;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Sheets.class)
+public class MixinSheets {
+	@Shadow
+	@Final
+	private static RenderType ARMOR_TRIMS_SHEET_TYPE;
+
+	@Inject(method = "<clinit>", at = @At("TAIL"))
+	private static void setSheet(CallbackInfo ci) {
+		((BlendingStateHolder) ARMOR_TRIMS_SHEET_TYPE).setTransparencyType(TransparencyType.GENERAL_TRANSPARENT);
+	}
+}
