16be8168b57363fe95abf1eb07e7ba5bfabf7ea4
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2023-01-26 21:30:50 -0800

    Use ModifyArgs to capture arguments

--- src/main/java/net/coderbot/iris/mixin/texture/pbr/MixinDirectoryLister.java
+++ src/main/java/net/coderbot/iris/mixin/texture/pbr/MixinDirectoryLister.java
@@ -7,37 +7,24 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import java.util.function.BiConsumer;
 
 @Mixin(DirectoryLister.class)
 public class MixinDirectoryLister {
-	@Unique
-	private static final ThreadLocal<ResourceManager> LOCAL_RESOURCE_MANAGER = new ThreadLocal<>();
-
-	@Inject(method = "run(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/client/renderer/texture/atlas/SpriteSource$Output;)V", at = @At("HEAD"))
-	private void iris$onHeadRun(ResourceManager resourceManager, SpriteSource.Output output, CallbackInfo ci) {
-		LOCAL_RESOURCE_MANAGER.set(resourceManager);
-	}
-
-	@ModifyArg(method = "run(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/client/renderer/texture/atlas/SpriteSource$Output;)V", at = @At(value = "INVOKE", target = "Ljava/util/Map;forEach(Ljava/util/function/BiConsumer;)V", remap = false, ordinal = 0), index = 0)
-	private BiConsumer<? super ResourceLocation, ? super Resource> iris$modifyForEachAction(BiConsumer<? super ResourceLocation, ? super Resource> action) {
-		ResourceManager resourceManager = LOCAL_RESOURCE_MANAGER.get();
-		LOCAL_RESOURCE_MANAGER.set(null);
-		if (resourceManager == null) {
-			return action;
-		}
-		return (location, resource) -> {
+	@ModifyArgs(method = "run(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/client/renderer/texture/atlas/SpriteSource$Output;)V", at = @At(value = "INVOKE", target = "Ljava/util/Map;forEach(Ljava/util/function/BiConsumer;)V", remap = false, ordinal = 0))
+	private void iris$modifyForEachAction(Args args, ResourceManager resourceManager, SpriteSource.Output output) {
+		BiConsumer<? super ResourceLocation, ? super Resource> action = args.get(0);
+		BiConsumer<? super ResourceLocation, ? super Resource> wrappedAction = (location, resource) -> {
 			ResourceLocation baseLocation = PBRType.removeSuffix(location);
 			if (baseLocation != null && resourceManager.getResource(baseLocation).isPresent()) {
 				return;
 			}
 			action.accept(location, resource);
 		};
+		args.set(0, wrappedAction);
 	}
 }
