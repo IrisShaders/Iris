c1b49353a8ed57041261921dceed902f09f5cadc
IMS212 <ims@irisshaders.dev>
2023-02-05 16:12:59 -0800

    Fix entity shadow POM

--- src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/vertex_format/entity/MixinEntityRenderDispatcher.java
+++ src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/vertex_format/entity/MixinEntityRenderDispatcher.java
@@ -10,6 +10,7 @@ import me.jellysquid.mods.sodium.client.util.color.ColorABGR;
 import me.jellysquid.mods.sodium.common.util.MatrixHelper;
 import net.coderbot.iris.compat.sodium.impl.vertex_format.entity_xhfp.EntityVertex;
 import net.coderbot.iris.vertices.ImmediateState;
+import net.coderbot.iris.vertices.NormalHelper;
 import net.irisshaders.iris.api.v0.IrisApi;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
@@ -49,22 +50,34 @@ public class MixinEntityRenderDispatcher {
 
 		boolean extended = shouldBeExtended();
 
+		int tangent = 0;
+
+		if (extended) {
+			tangent = getTangent(normal, minX, minY, minZ, u1, v1,
+				minX, minY, maxZ, u1, v2,
+				maxX, minY, maxZ, u2, v2
+			);
+		}
+
+		float midU = (u1 + u2) / 2;
+		float midV = (v1 + v2) / 2;
+
 		int stride = extended ? EntityVertex.STRIDE : ModelVertex.STRIDE;
 		try (MemoryStack stack = RenderGlobal.VERTEX_DATA.push()) {
 			long buffer = stack.nmalloc(4 * stride);
 			long ptr = buffer;
 
 			if (extended) {
-				writeShadowVertexIris(ptr, matPosition, minX, minY, minZ, u1, v1, color, normal);
+				writeShadowVertexIris(ptr, matPosition, minX, minY, minZ, u1, v1, color, midU, midV, normal, tangent);
 				ptr += stride;
 
-				writeShadowVertexIris(ptr, matPosition, minX, minY, maxZ, u1, v2, color, normal);
+				writeShadowVertexIris(ptr, matPosition, minX, minY, maxZ, u1, v2, color, midU, midV, normal, tangent);
 				ptr += stride;
 
-				writeShadowVertexIris(ptr, matPosition, maxX, minY, maxZ, u2, v2, color, normal);
+				writeShadowVertexIris(ptr, matPosition, maxX, minY, maxZ, u2, v2, color, midU, midV, normal, tangent);
 				ptr += stride;
 
-				writeShadowVertexIris(ptr, matPosition, maxX, minY, minZ, u2, v1, color, normal);
+				writeShadowVertexIris(ptr, matPosition, maxX, minY, minZ, u2, v1, color, midU, midV, normal, tangent);
 				ptr += stride;
 			} else {
 				writeShadowVertex(ptr, matPosition, minX, minY, minZ, u1, v1, color, normal);
@@ -85,13 +98,13 @@ public class MixinEntityRenderDispatcher {
 		}
 	}
 
-	private static void writeShadowVertexIris(long ptr, Matrix4f matPosition, float x, float y, float z, float u, float v, int color, int normal) {
+	private static void writeShadowVertexIris(long ptr, Matrix4f matPosition, float x, float y, float z, float u, float v, int color, float midU, float midV, int normal, int tangent) {
 		// The transformed position vector
 		float xt = MatrixHelper.transformPositionX(matPosition, x, y, z);
 		float yt = MatrixHelper.transformPositionY(matPosition, x, y, z);
 		float zt = MatrixHelper.transformPositionZ(matPosition, x, y, z);
 
-		EntityVertex.write(ptr, xt, yt, zt, color, u, v, 0.5f, 0.5f, OverlayTexture.NO_OVERLAY, LightTexture.FULL_BRIGHT, normal, 0);
+		EntityVertex.write(ptr, xt, yt, zt, color, u, v, midU, midV, OverlayTexture.NO_OVERLAY, LightTexture.FULL_BRIGHT, normal, tangent);
 	}
 
 	private static void writeShadowVertex(long ptr, Matrix4f matPosition, float x, float y, float z, float u, float v, int color, int normal) {
@@ -105,5 +118,86 @@ public class MixinEntityRenderDispatcher {
 
 	private static boolean shouldBeExtended() {
 		return IrisApi.getInstance().isShaderPackInUse() && ImmediateState.renderWithExtendedVertexFormat;
+	}
+
+	private static int getTangent(int normal, float x0, float y0, float z0, float u0, float v0, float x1, float y1, float z1, float u1, float v1, float x2, float y2, float z2, float u2, float v2) {
+		// Capture all of the relevant vertex positions
+
+		float normalX = Norm3b.unpackX(normal);
+		float normalY = Norm3b.unpackY(normal);
+		float normalZ = Norm3b.unpackZ(normal);
+
+		float edge1x = x1 - x0;
+		float edge1y = y1 - y0;
+		float edge1z = z1 - z0;
+
+		float edge2x = x2 - x0;
+		float edge2y = y2 - y0;
+		float edge2z = z2 - z0;
+
+		float deltaU1 = u1 - u0;
+		float deltaV1 = v1 - v0;
+		float deltaU2 = u2 - u0;
+		float deltaV2 = v2 - v0;
+
+		float fdenom = deltaU1 * deltaV2 - deltaU2 * deltaV1;
+		float f;
+
+		if (fdenom == 0.0) {
+			f = 1.0f;
+		} else {
+			f = 1.0f / fdenom;
+		}
+
+		float tangentx = f * (deltaV2 * edge1x - deltaV1 * edge2x);
+		float tangenty = f * (deltaV2 * edge1y - deltaV1 * edge2y);
+		float tangentz = f * (deltaV2 * edge1z - deltaV1 * edge2z);
+		float tcoeff = rsqrt(tangentx * tangentx + tangenty * tangenty + tangentz * tangentz);
+		tangentx *= tcoeff;
+		tangenty *= tcoeff;
+		tangentz *= tcoeff;
+
+		float bitangentx = f * (-deltaU2 * edge1x + deltaU1 * edge2x);
+		float bitangenty = f * (-deltaU2 * edge1y + deltaU1 * edge2y);
+		float bitangentz = f * (-deltaU2 * edge1z + deltaU1 * edge2z);
+		float bitcoeff = rsqrt(bitangentx * bitangentx + bitangenty * bitangenty + bitangentz * bitangentz);
+		bitangentx *= bitcoeff;
+		bitangenty *= bitcoeff;
+		bitangentz *= bitcoeff;
+
+		// predicted bitangent = tangent × normal
+		// Compute the determinant of the following matrix to get the cross product
+		//  i  j  k
+		// tx ty tz
+		// nx ny nz
+
+		// Be very careful when writing out complex multi-step calculations
+		// such as vector cross products! The calculation for pbitangentz
+		// used to be broken because it multiplied values in the wrong order.
+
+		float pbitangentx = tangenty * normalZ - tangentz * normalY;
+		float pbitangenty = tangentz * normalX - tangentx * normalZ;
+		float pbitangentz = tangentx * normalY - tangenty * normalX;
+
+		float dot = (bitangentx * pbitangentx) + (bitangenty * pbitangenty) + (bitangentz * pbitangentz);
+		float tangentW;
+
+		if (dot < 0) {
+			tangentW = -1.0F;
+		} else {
+			tangentW = 1.0F;
+		}
+
+		return NormalHelper.packNormal(tangentx, tangenty, tangentz, tangentW);
+	}
+
+	private static float rsqrt(float value) {
+		if (value == 0.0f) {
+			// You heard it here first, folks: 1 divided by 0 equals 1
+			// In actuality, this is a workaround for normalizing a zero length vector (leaving it as zero length)
+			return 1.0f;
+		} else {
+			return (float) (1.0 / Math.sqrt(value));
+		}
 	}
 }
