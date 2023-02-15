4ed9fb13fae540f37a38026977ce2290016014b6
IMS212 <ims@irisshaders.dev>
2023-02-15 08:00:40 -0800

    Modify pack screen to load faster

--- src/main/java/net/coderbot/iris/Iris.java
+++ src/main/java/net/coderbot/iris/Iris.java
@@ -316,6 +316,11 @@ public class Iris {
 			return false;
 		}
 
+		if (!isValidShaderpack(shaderPackRoot)) {
+			logger.error("Pack \"{}\" is not valid! Can't load it.", name);
+			return false;
+		}
+
 		Path shaderPackPath;
 
 		if (shaderPackRoot.toString().endsWith(".zip")) {
@@ -491,6 +496,10 @@ public class Iris {
 		} catch (IOException e) {
 			// TODO: Better error handling
 		}
+	}
+
+	public static boolean isValidToShowPack(Path pack) {
+		return Files.isDirectory(pack) || pack.toString().endsWith(".zip");
 	}
 
 	public static boolean isValidShaderpack(Path pack) {
