79aa0e5bda6e7090c90f054fe62c8b549716bca5
IMS212 <ims@irisshaders.dev>
2023-04-04 19:13:07 -0700

    BlockItem support

--- src/main/java/net/coderbot/iris/mixin/entity_render_context/MixinItemRenderer.java
+++ src/main/java/net/coderbot/iris/mixin/entity_render_context/MixinItemRenderer.java
@@ -12,8 +12,10 @@ import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -21,18 +23,31 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ItemRenderer.class)
 public abstract class MixinItemRenderer {
+	@Unique
+	private int previousBeValue;
 
 	@Inject(method = "render", at = @At(value = "HEAD"))
 	private void changeId(ItemStack pItemRenderer0, ItemTransforms.TransformType pItemTransforms$TransformType1, boolean pBoolean2, PoseStack pPoseStack3, MultiBufferSource pMultiBufferSource4, int pInt5, int pInt6, BakedModel pBakedModel7, CallbackInfo ci) {
 		if (BlockRenderingSettings.INSTANCE.getItemIds() == null) return;
 
-		ResourceLocation location = Registry.ITEM.getKey(pItemRenderer0.getItem());
+		if (pItemRenderer0.getItem() instanceof BlockItem blockItem) {
+			if (BlockRenderingSettings.INSTANCE.getBlockStateIds() == null) return;
 
-		CapturedRenderingState.INSTANCE.setCurrentRenderedItem(BlockRenderingSettings.INSTANCE.getItemIds().applyAsInt(new NamespacedId(location.getNamespace(), location.getPath())));
+			previousBeValue = CapturedRenderingState.INSTANCE.getCurrentRenderedBlockEntity();
+			CapturedRenderingState.INSTANCE.setCurrentBlockEntity(1);
+
+			CapturedRenderingState.INSTANCE.setCurrentRenderedItem(BlockRenderingSettings.INSTANCE.getBlockStateIds().getOrDefault(blockItem.getBlock().defaultBlockState(), 0));
+		} else {
+			ResourceLocation location = Registry.ITEM.getKey(pItemRenderer0.getItem());
+
+			CapturedRenderingState.INSTANCE.setCurrentRenderedItem(BlockRenderingSettings.INSTANCE.getItemIds().applyAsInt(new NamespacedId(location.getNamespace(), location.getPath())));
+		}
 	}
 
 	@Inject(method = "render", at = @At(value = "TAIL"))
 	private void changeId2(CallbackInfo ci) {
 		CapturedRenderingState.INSTANCE.setCurrentRenderedItem(0);
+		CapturedRenderingState.INSTANCE.setCurrentBlockEntity(previousBeValue);
+		previousBeValue = 0;
 	}
 }
