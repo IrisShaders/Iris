2dc92a0c89f42abb398d7e79ed6ca8a1fd4c28ca
IMS212 <ims@irisshaders.dev>
2023-04-06 13:50:52 -0700

    Enable held light color

--- src/main/java/net/coderbot/iris/uniforms/IdMapUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/IdMapUniforms.java
@@ -37,10 +37,9 @@ public final class IdMapUniforms {
 			.uniform1i(UniformUpdateFrequency.PER_FRAME, "heldItemId", mainHandSupplier::getIntID)
 			.uniform1i(UniformUpdateFrequency.PER_FRAME, "heldItemId2", offHandSupplier::getIntID)
 			.uniform1i(PER_FRAME, "heldBlockLightValue", mainHandSupplier::getLightValue)
-			.uniform1i(PER_FRAME, "heldBlockLightValue2", offHandSupplier::getLightValue);
-		// TODO: Figure out API.
-			//.uniformVanilla3f(PER_FRAME, "heldBlockLightColor", mainHandSupplier::getLightColor)
-			//.uniformVanilla3f(PER_FRAME, "heldBlockLightColor2", offHandSupplier::getLightColor);
+			.uniform1i(PER_FRAME, "heldBlockLightValue2", offHandSupplier::getLightValue)
+			.uniformVanilla3f(PER_FRAME, "heldBlockLightColor", mainHandSupplier::getLightColor)
+			.uniformVanilla3f(PER_FRAME, "heldBlockLightColor2", offHandSupplier::getLightColor);
 	}
 
 	/**
