eda58214878b8407915fb9dc877c5fcacb0f2951
IMS212 <ims@irisshaders.dev>
2023-07-09 14:26:43 -0700

    Fix Indium 1.0.20

--- src/main/java/net/coderbot/iris/compat/indium/mixin/MixinAbstractBlockRenderContext.java
+++ src/main/java/net/coderbot/iris/compat/indium/mixin/MixinAbstractBlockRenderContext.java
@@ -4,36 +4,20 @@ import net.coderbot.iris.block_rendering.BlockRenderingSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * Small tweak to Indium to make it obey our separateAo setting.
  */
-@Mixin(targets = "link/infra/indium/renderer/render/BaseQuadRenderer", remap = false)
+@Mixin(targets = "link/infra/indium/renderer/render/AbstractBlockRenderContext", remap = false)
 @Pseudo
-public class MixinBaseQuadRenderer {
+public class MixinAbstractBlockRenderContext {
 	// One of these injections must pass, or else the game will crash.
-	@Group(name = "iris_separateIndiumAO", min = 2, max = 4)
-	@Redirect(method = {"tessellateSmooth", "tessellateSmoothEmissive", "shadeFlatQuad"},
+	//@Group(name = "iris_separateIndiumAO", min = 2, max = 3)
+	@Redirect(method = {"shadeQuad", "shadeFlatQuad"},
 		at = @At(value = "INVOKE",
 			target = "Llink/infra/indium/renderer/helper/ColorHelper;multiplyRGB(IF)I"), require = 0)
 	private int iris$separateAoColorMultiply(int color, float ao) {
-		if (BlockRenderingSettings.INSTANCE.shouldUseSeparateAo()) {
-			color &= 0x00FFFFFF;
-			color |= ((int) (ao * 255.0f)) << 24;
-
-			return color;
-		} else {
-			return iris$multiplyRGB(color, ao);
-		}
-	}
-
-	@Group(name = "iris_separateIndiumAO", min = 2, max = 4)
-	@Redirect(method = {"tesselateSmooth", "tesselateSmoothEmissive"},
-		at = @At(value = "INVOKE",
-			target = "Llink/infra/indium/renderer/helper/ColorHelper;multiplyRGB(IF)I"), require = 0)
-	private int iris$separateAoColorMultiplyOld(int color, float ao) {
 		if (BlockRenderingSettings.INSTANCE.shouldUseSeparateAo()) {
 			color &= 0x00FFFFFF;
 			color |= ((int) (ao * 255.0f)) << 24;
