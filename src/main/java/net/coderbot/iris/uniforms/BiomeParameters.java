d8d56d82a88e6dea2947483d55183fca10d90ae4
IMS212 <ims@irisshaders.dev>
2023-10-12 11:34:12 -0700

    Fix build 2

--- src/main/java/net/coderbot/iris/uniforms/BiomeParameters.java
+++ src/main/java/net/coderbot/iris/uniforms/BiomeParameters.java
@@ -10,7 +10,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
