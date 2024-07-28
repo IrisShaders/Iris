c2a862f9cf8d0db36e14ddf9871490ed5976508c
IMS212 <ims@irisshaders.dev>
2024-07-28 12:40:27 -0700

    Fix glass shadows when using XyCraft Override

--- common/src/main/java/net/irisshaders/iris/shaderpack/materialmap/BlockMaterialMapping.java
+++ common/src/main/java/net/irisshaders/iris/shaderpack/materialmap/BlockMaterialMapping.java
@@ -101,7 +101,7 @@ public class BlockMaterialMapping {
 	}
 
 	public static Map<Block, BlockRenderType> createBlockTypeMap(Map<NamespacedId, BlockRenderType> blockPropertiesMap) {
-		Map<Block, BlockRenderType> blockTypeIds = new Reference2ReferenceLinkedOpenHashMap<>();
+		Map<Block, BlockRenderType> blockTypeIds = new Reference2ReferenceOpenHashMap<>();
 
 		blockPropertiesMap.forEach((id, blockType) -> {
 			ResourceLocation resourceLocation = new ResourceLocation(id.getNamespace(), id.getName());
