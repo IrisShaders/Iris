1be776af2247739e5d68d0f8fce6e3b459927f38
douira <douira100@gmail.com>
2024-12-31 05:51:28 +0100

    Simplify iris and sodium integration by taking advantage of sodium's version of the maintained removable section tree

--- common/src/main/java/net/irisshaders/iris/compat/sodium/mixin/MixinRenderSectionManagerShadow.java
+++ common/src/main/java/net/irisshaders/iris/compat/sodium/mixin/MixinRenderSectionManagerShadow.java
@@ -3,16 +3,15 @@ package net.irisshaders.iris.compat.sodium.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.caffeinemc.mods.sodium.client.render.chunk.RenderSection;
-import net.caffeinemc.mods.sodium.client.render.chunk.RenderSectionFlags;
 import net.caffeinemc.mods.sodium.client.render.chunk.RenderSectionManager;
 import net.caffeinemc.mods.sodium.client.render.chunk.data.BuiltSectionInfo;
 import net.caffeinemc.mods.sodium.client.render.chunk.lists.SortedRenderLists;
 import net.caffeinemc.mods.sodium.client.render.chunk.lists.VisibleChunkCollectorAsync;
 import net.caffeinemc.mods.sodium.client.render.chunk.region.RenderRegionManager;
+import net.caffeinemc.mods.sodium.client.render.chunk.tree.RemovableMultiForest;
 import net.caffeinemc.mods.sodium.client.render.viewport.Viewport;
 import net.irisshaders.iris.compat.sodium.mixinterface.ShadowRenderRegion;
 import net.irisshaders.iris.shadows.ShadowRenderingState;
-import net.irisshaders.iris.shadows.ShadowSectionTree;
 import net.minecraft.client.Camera;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
@@ -46,9 +45,6 @@ public abstract class MixinRenderSectionManagerShadow {
 	private boolean shadowNeedsRenderListUpdate = true;
 
 	@Unique
-	private ShadowSectionTree shadowSectionTree;
-
-	@Unique
 	private boolean renderListStateIsShadow = false;
 
 	@WrapOperation(method = "notifyChangedCamera", at = @At(value = "FIELD", target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/RenderSectionManager;cameraChanged:Z"))
@@ -67,18 +63,20 @@ public abstract class MixinRenderSectionManagerShadow {
 	}
 
 	@Shadow(remap = false)
-	protected abstract float getRenderDistance();
-
-	@Shadow(remap = false)
 	public abstract int getVisibleChunkCount();
 
 	@Shadow(remap = false)
 	public abstract int getTotalSections();
 
+	@Shadow
+	@Final
+	private RemovableMultiForest renderableSectionTree;
+
+	@Shadow
+	protected abstract float getSearchDistance();
+
 	@Inject(method = "updateRenderLists", at = @At("HEAD"), cancellable = true)
 	private void updateShadowRenderLists(Camera camera, Viewport viewport, boolean spectator, boolean updateImmediately, CallbackInfo ci) {
-		this.ensureShadowSectionTree();
-
 		if (!ShadowRenderingState.areShadowsCurrentlyBeingRendered()) {
 			if (this.renderListStateIsShadow) {
 				for (var region : this.regions.getLoadedRegions()) {
@@ -98,43 +96,22 @@ public abstract class MixinRenderSectionManagerShadow {
 			}
 
 			var visibleCollector = new VisibleChunkCollectorAsync(this.regions, -this.frame);
-			this.shadowSectionTree.prepareTreesForTraversal();
-			this.shadowSectionTree.traverseVisible(visibleCollector, viewport);
-			this.shadowRenderLists = visibleCollector.createRenderLists();
+			this.renderableSectionTree.prepareForTraversal();
+			this.renderableSectionTree.traverse(visibleCollector, viewport, this.getSearchDistance());
+			this.shadowRenderLists = visibleCollector.createRenderLists(viewport);
 		}
 
 		this.shadowNeedsRenderListUpdate = false;
 		ci.cancel();
 	}
 
-	@Unique
-	private void ensureShadowSectionTree() {
-		if (this.shadowSectionTree == null) {
-			this.shadowSectionTree = new ShadowSectionTree(this.getRenderDistance());
-		}
-	}
-
 	@Inject(method = "updateSectionInfo", at = @At("HEAD"))
-	private void updateSectionInfo(RenderSection render, BuiltSectionInfo info, CallbackInfo ci) {
-		this.ensureShadowSectionTree();
-
-		var x = render.getChunkX();
-		var y = render.getChunkY();
-		var z = render.getChunkZ();
-
-		if (info == null || (info.flags & RenderSectionFlags.MASK_NEEDS_RENDER) == 0) {
-			this.shadowSectionTree.removeSection(x, y, z);
-		} else {
-			this.shadowSectionTree.addSection(x, y, z);
-		}
+	private void updateSectionInfo(RenderSection render, BuiltSectionInfo info, CallbackInfoReturnable<Boolean> cir) {
 		this.shadowNeedsRenderListUpdate = true;
 	}
 
 	@Inject(method = "onSectionRemoved", at = @At("HEAD"))
 	private void onSectionRemoved(int x, int y, int z, CallbackInfo ci) {
-		this.ensureShadowSectionTree();
-
-		this.shadowSectionTree.removeSection(x, y, z);
 		this.shadowNeedsRenderListUpdate = true;
 	}
 
