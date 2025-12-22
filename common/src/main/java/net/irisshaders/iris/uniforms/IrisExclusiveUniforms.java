a212101d8f6b381cafe5b7f81eb35894da51640b
IMS <ims@irisshaders.dev>
2025-12-22 10:50:41 -0800

    rename uniforms

--- common/src/main/java/net/irisshaders/iris/uniforms/IrisExclusiveUniforms.java
+++ common/src/main/java/net/irisshaders/iris/uniforms/IrisExclusiveUniforms.java
@@ -51,10 +51,10 @@ public class IrisExclusiveUniforms {
 		});
 
 		//All Iris-exclusive uniforms (uniforms which do not exist in either OptiFine or ShadersMod) should be registered here.
-		uniforms.uniform1b(UniformUpdateFrequency.PER_TICK, "is_inShallowWater", IrisExclusiveUniforms::getIsInShallowWater);
-		uniforms.uniform1b(UniformUpdateFrequency.PER_TICK, "is_swimming", IrisExclusiveUniforms::getIsSwimming);
-		uniforms.uniform1b(UniformUpdateFrequency.PER_TICK, "is_passenger", IrisExclusiveUniforms::getIsPassenger);
-		uniforms.uniform1b(UniformUpdateFrequency.PER_TICK, "vehicleInShallowWater", IrisExclusiveUniforms::getVehicleInShallowWater);
+		uniforms.uniform1b(UniformUpdateFrequency.PER_TICK, "feetInWater", IrisExclusiveUniforms::getIsInShallowWater);
+		uniforms.uniform1b(UniformUpdateFrequency.PER_TICK, "inSwimmingAnimation", IrisExclusiveUniforms::getIsSwimming);
+		uniforms.uniform1b(UniformUpdateFrequency.PER_TICK, "isRiding", IrisExclusiveUniforms::getIsPassenger);
+		uniforms.uniform1b(UniformUpdateFrequency.PER_TICK, "vehicleInWater", IrisExclusiveUniforms::getVehicleInShallowWater);
 		uniforms.uniform1i(UniformUpdateFrequency.PER_TICK, "vehicleId", IrisExclusiveUniforms::getVehicleId);
 		uniforms.uniform3d(PER_FRAME, "vehicleLookVector", IrisExclusiveUniforms::getVehicleLookVector);
 		uniforms.uniform3d(PER_FRAME, "relativeVehiclePosition", IrisExclusiveUniforms::getRelativeVehiclePosition);
