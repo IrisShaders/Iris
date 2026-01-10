0977ef83f3fe9baac2ccf36145666c32308c3247
lipesto <vovan656a4@mail.ru>
2026-01-10 10:53:26 +0700

    f

--- common/src/main/java/net/irisshaders/iris/pipeline/transform/transformer/SodiumTransformer.java
+++ common/src/main/java/net/irisshaders/iris/pipeline/transform/transformer/SodiumTransformer.java
@@ -149,7 +149,7 @@ vec3 oct_to_vec3(vec2 e) {
 			"""
 vec4 tangent_decode(vec2 e) {
 	vec2 oct_compressed = e;
-	float r_sign = oct_compressed.y > 0.0 ? 1.0 : -1.0;
+	float r_sign = oct_compressed.y > 0.0f ? 1.0f : -1.0f;
 	oct_compressed.y = oct_compressed.y * (127.0f / 63.0f) + r_sign * (-64.0f / 63.0f);
 	vec3 res = oct_to_vec3(oct_compressed.xy);
 	return vec4(res, r_sign);
