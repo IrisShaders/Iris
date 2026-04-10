663d73b74c30a5fb62263f7be550f1b2674a8615
djmrFunnyMan <68905776+djmrFunnyMan@users.noreply.github.com>
2026-04-11 01:01:51 +0200

    Add (uncomment) clamping for end flash angle
    A 1 degree clamp is inadequate as shadows are basically parallel to the horizon. 10 degrees produce much more stable results.

--- common/src/main/java/net/irisshaders/iris/mixin/MixinEndFlash.java
+++ common/src/main/java/net/irisshaders/iris/mixin/MixinEndFlash.java
@@ -22,14 +22,13 @@ public class MixinEndFlash {
 	@Shadow
 	private long flashSeed;
 
-	private static final float ABOVE_HORIZON_EPS = 1.0F; // degrees above horizon
+	private static final float ABOVE_HORIZON_EPS = 10.0F; // degrees above horizon
 
-	// Working on it...
-	//@Inject(method = "calculateFlashParameters", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;randomBetween(Lnet/minecraft/util/RandomSource;FF)F", ordinal = 0), cancellable = true)
+	@Inject(method = "calculateFlashParameters", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;randomBetween(Lnet/minecraft/util/RandomSource;FF)F", ordinal = 0), cancellable = true)
 	private void iris$calculateNewAngles(long l, CallbackInfo ci, @Local(ordinal = 1) long m, @Local RandomSource randomSource) {
 		if (Iris.getCurrentPack().isPresent()) {
 			ci.cancel();
-			this.xAngle = -Mth.randomBetween(randomSource, ABOVE_HORIZON_EPS, 60.0F); // [-60, -1]
+			this.xAngle = -Mth.randomBetween(randomSource, ABOVE_HORIZON_EPS, 60.0F); // [-60, -10]
 			this.yAngle = Mth.randomBetween(randomSource, -180.0F, 180.0F);
 			this.flashSeed = m;
 		}
