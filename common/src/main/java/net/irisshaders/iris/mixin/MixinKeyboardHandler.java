e10d585e7ee7f2be2cba8b9e4c7365b6abb903a4
GeForceLegend <41003546+GeForceLegend@users.noreply.github.com>
2026-07-18 11:39:16 +0800

    Fixed other debug key bindings

--- common/src/main/java/net/irisshaders/iris/mixin/MixinKeyboardHandler.java
+++ common/src/main/java/net/irisshaders/iris/mixin/MixinKeyboardHandler.java
@@ -13,6 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinKeyboardHandler {
     @Inject(method = "handleDebugKeys", at = @At("RETURN"), cancellable = true)
     private void iris$handleDebugKeys(KeyEvent event, CallbackInfoReturnable<Boolean> cir) {
-		cir.setReturnValue(Iris.handleDebugKeys(event));
+		if (Iris.handleDebugKeys(event)) {
+			cir.setReturnValue(true);
+		}
     }
 }
