6369373613646808701cb20222ae69fab50f912d
IMS212 <ims@irisshaders.dev>
2026-09-11 21:49:27 -0700

    Update to Sodium 0.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,15 @@ val NEOFORGE_VERSION by extra { "26.1.2.10-beta" }
 val FABRIC_LOADER_VERSION by extra { "0.19.3" }
 val FABRIC_API_VERSION by extra { "0.154.2+26.1.2" }
 
-val SODIUM_DEPENDENCY_FABRIC by extra { "net.caffeinemc:sodium-fabric:0.9.1+mc26.1.2" }
-val SODIUM_DEPENDENCY_NEO by extra { "net.caffeinemc:sodium-neoforge-mod:0.9.1+mc26.1.2" }
+val SODIUM_DEPENDENCY_FABRIC by extra { "net.caffeinemc:sodium-fabric:0.9.2+mc26.1.2" }
+val SODIUM_DEPENDENCY_NEO by extra { "net.caffeinemc:sodium-neoforge-mod:0.9.2+mc26.1.2" }
 
 // This value can be set to null to disable Parchment.
 // TODO: Re-add Parchment
 val PARCHMENT_VERSION by extra { null }
 
 // https://semver.org/
-val MOD_VERSION by extra { "1.11.2" }
+val MOD_VERSION by extra { "1.11.4" }
 
 allprojects {
     apply(plugin = "java")
